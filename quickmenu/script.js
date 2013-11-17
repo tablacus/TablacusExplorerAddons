7e793304b295de1f07ce2b25e71f93e4643c6c52
Gaku <rxf10242@gmail.com>
2013-11-17 22:30:37 +0900

    13.11.17

---
+++ quickmenu/script.js
@@ -0,0 +1,150 @@
+﻿if (window.Addon == 1) {
+
+	Addons.QuickMenu =
+	{
+		Items: null,
+		SendTo: function (hMenu)
+		{
+			hMenu = api.sscanf(hMenu, "%llx");
+			var wID = 0x6000;
+			var fol = sha.NameSpace(api.GetDisplayNameOf(ssfSENDTO, SHGDN_FORPARSING));
+			Addons.QuickMenu.Items = fol.Items();
+			for (var i = 0; i < Addons.QuickMenu.Items.Count; i++) {
+				ExtraMenuCommand[wID] = function (Ctrl, pt, Name, nVerb)
+				{
+					var DropTarget = api.DropTarget(Addons.QuickMenu.Items.Item(nVerb - 0x6000));
+					pdwEffect = api.Memory("DWORD");
+					pdwEffect[0] = DROPEFFECT_COPY | DROPEFFECT_MOVE | DROPEFFECT_LINK;
+					var Selected = Ctrl.SelectedItems();
+					if (Selected) {
+						DropTarget.Drop(Selected, MK_LBUTTON, pt, pdwEffect);
+					}
+				};
+				api.InsertMenu(hMenu, MAXINT, MF_BYPOSITION | MF_STRING, wID++, api.GetDisplayNameOf(Addons.QuickMenu.Items.Item(i), SHGDN_INFOLDER));
+			}
+		}
+	}
+
+	AddEvent("GetBaseMenu", function (nBase, FV, Selected, uCMF, Mode)
+	{
+		var hMenu;
+		var nPos = 0;
+		if (Mode & 0x10) {
+			return;
+		}
+		switch (nBase) {
+			case 2:
+				hMenu = te.MainMenu(FCIDM_MENU_FILE);
+				var hModule = api.LoadLibraryEx(fso.BuildPath(system32, "shell32.dll"), 0, LOAD_LIBRARY_AS_DATAFILE);
+				var hMenu1 = api.LoadMenu(hModule, 233);
+				var hMenu2 = api.GetSubMenu(hMenu1, 0);
+
+				var mii = api.Memory("MENUITEMINFO");
+				mii.cbSize = mii.Size;
+				mii.fMask = MIIM_STRING | MIIM_STATE | MIIM_ID;
+				mii.fState = MFS_DEFAULT;
+				mii.wID = nPos + 0x1000;
+				mii.dwTypeData = api.GetMenuString(hMenu2, 0, MF_BYPOSITION);
+				ExtraMenuCommand[mii.wID] = function (Ctrl, pt, Name, nVerb)
+				{
+					if (ExecMenu(Ctrl, "Default", pt, 2) != S_OK) {
+						InvokeCommand(Ctrl.SelectedItems(), 0, te.hwnd, null, null, null, SW_SHOWNORMAL, 0, 0);
+					}
+				};
+				api.InsertMenuItem(hMenu, nPos++, true, mii);
+				api.DestroyMenu(hMenu2);
+				api.DestroyMenu(hMenu1);
+
+				mii.wID = nPos + 0x1000;
+				ExtraMenuCommand[mii.wID] = function (Ctrl, pt, Name, nVerb)
+				{
+					InvokeCommand(Ctrl.SelectedItems(), 0, te.hwnd, "Edit", null, null, SW_SHOWNORMAL, 0, 0);
+				};
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_STRING, mii.wID, GetText("Edit"));
+
+				mii.wID = nPos + 0x1000;
+				ExtraMenuCommand[mii.wID] = function (Ctrl, pt, Name, nVerb)
+				{
+					ExecMenu(Ctrl, Name, pt, 0x10);
+				};
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_STRING, mii.wID, GetText("Default"));
+
+				hMenu1 = api.LoadMenu(hModule, 223);
+				hMenu2 = api.GetSubMenu(hMenu1, 0);
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_SEPARATOR, 0, null);
+				mii.fMask  = MIIM_ID | MIIM_STRING | MIIM_SUBMENU;
+				mii.wID = nPos + 0x1000;
+				mii.dwTypeData = api.GetMenuString(hMenu2, 0, MF_BYPOSITION);
+				mii.hSubMenu = api.CreatePopupMenu();
+				api.InsertMenu(mii.hSubMenu, 0, MF_BYPOSITION | MF_STRING, 0, api.sprintf(100, '\tJScript\tAddons.QuickMenu.SendTo("%llx")', mii.hSubMenu));
+				api.InsertMenuItem(hMenu, nPos++, true, mii);
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_SEPARATOR, 0, null);
+				api.DestroyMenu(hMenu2);
+				api.DestroyMenu(hMenu1);
+
+				hMenu1 = api.LoadMenu(hModule, 210);
+				hMenu2 = api.GetSubMenu(hMenu1, 0);
+
+				mii.wID = nPos + 0x1000;
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_STRING, mii.wID, api.GetMenuString(hMenu2, 0, MF_BYPOSITION));
+				ExtraMenuCommand[mii.wID] = function (Ctrl, pt, Name, nVerb)
+				{
+					InvokeCommand(Ctrl.SelectedItems(), 0, te.hwnd, "Cut", null, null, SW_SHOWNORMAL, 0, 0);
+				};
+
+				mii.wID = nPos + 0x1000;
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_STRING, mii.wID, api.GetMenuString(hMenu2, 1, MF_BYPOSITION));
+				ExtraMenuCommand[mii.wID] = function (Ctrl, pt, Name, nVerb)
+				{
+					InvokeCommand(Ctrl.SelectedItems(), 0, te.hwnd, "Copy", null, null, SW_SHOWNORMAL, 0, 0);
+				};
+
+				mii.wID = nPos + 0x1000;
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_STRING, mii.wID, api.GetMenuString(hMenu2, 2, MF_BYPOSITION));
+				ExtraMenuCommand[mii.wID] = function (Ctrl, pt, Name, nVerb)
+				{
+					var Items = Ctrl.SelectedItems();
+					if (Items.Count == 1 && Items.Item(0).IsFolder) {
+					}
+					else {
+						Items = Ctrl;
+					}
+					InvokeCommand(Items, 0, te.hwnd, "Paste", null, null, SW_SHOWNORMAL, 0, 0);
+				};
+				api.DestroyMenu(hMenu2);
+				api.DestroyMenu(hMenu1);
+				api.FreeLibrary(hModule);
+
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_SEPARATOR, 0, null);
+				return [hMenu];
+			case 4:
+				hMenu = te.MainMenu(FCIDM_MENU_FILE);
+				var wID = nPos + 0x1000;
+				ExtraMenuCommand[wID] = function (Ctrl, pt, Name, nVerb)
+				{
+					ExecMenu(Ctrl, Name, pt, 0x10);
+				};
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_STRING, wID, GetText("Default"));
+				return [hMenu];
+			case 5:
+				hMenu = te.MainMenu(FCIDM_MENU_EDIT);
+				var wID = nPos + 0x1000;
+				ExtraMenuCommand[wID] = function (Ctrl, pt, Name, nVerb)
+				{
+					ExecMenu(Ctrl, Name, pt, 0x10);
+				};
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_STRING, wID, GetText("Default"));
+				return [hMenu];
+			case 6:
+				hMenu = te.MainMenu(FCIDM_MENU_VIEW);
+				var wID = nPos + 0x1000;
+				ExtraMenuCommand[wID] = function (Ctrl, pt, Name, nVerb)
+				{
+					ExecMenu(Ctrl, Name, pt, 0x10);
+				};
+				api.InsertMenu(hMenu, nPos++, MF_BYPOSITION | MF_STRING, wID, GetText("Default"));
+				return [hMenu];
+		}
+	});
+}
+
---
+++ speeddial/script.js
@@ -0,0 +1,78 @@
+﻿if (window.Addon == 1) {
+	Addons.SpeedDial =
+	{
+		SAVE: 512,
+		DISP: 10,
+		PATH: fso.BuildPath(te.Data.DataFolder, "newtab"),
+		CONFIG: fso.BuildPath(te.Data.DataFolder, "config\\speeddial.tsv"),
+		db: [],
+		bSave: false
+	}
+	try {
+		var f = fso.OpenTextFile(Addons.SpeedDial.CONFIG, 1, false, -1);
+		var s;
+		while (s = f.ReadLine()) {
+			Addons.SpeedDial.db.push(s);
+		}
+		f.Close();
+	}
+	catch (e) {
+		f && f.Close();
+	}
+
+	AddEvent("SaveConfig", function ()
+	{
+		if (Addons.SpeedDial.bSave) {
+			try {
+				var f = fso.OpenTextFile(Addons.SpeedDial.CONFIG, 2, true, -1);
+				for (var i in Addons.SpeedDial.db) {
+					f.WriteLine(Addons.SpeedDial.db[i]);
+				}
+				f.Close();
+			}
+			catch (e) {}
+		}
+	});
+
+	AddEvent("ListViewCreated", function (Ctrl)
+	{
+		if (api.ILIsEqual(Ctrl.FolderItem, Addons.SpeedDial.PATH)) {
+			api.SHFileOperation(FO_DELETE, fso.BuildPath(Addons.SpeedDial.PATH, "*.lnk"), null, FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_SILENT, false);
+			var hash = {};
+			for (var i in Addons.SpeedDial.db) {
+				hash[Addons.SpeedDial.db[i]] = api.QuadPart(hash[Addons.SpeedDial.db[i]]) + 1;
+			}
+			var keys = [];
+			for (var i in hash) {
+				keys.push(i);
+			}
+			keys.sort(function (a, b) {
+				return hash[b] - hash[a];
+			});
+			keys.splice(Addons.SpeedDial.DISP, MAXINT);
+			for (var i = 0; i < keys.length; i ++) {
+				try {
+					var ar = keys[i].split("\t");
+					var sc = wsh.CreateShortcut(fso.BuildPath(Addons.SpeedDial.PATH, api.sprintf(9, "%02d_", i + 1) + ar[1].replace(/:/g, "") + ".lnk"));
+					sc.TargetPath = ar[0] == "0" ? "Shell:Desktop" : ar[0];
+					sc.Save();
+				} catch (e) {
+				}
+			}
+		}
+		else {
+			var path = api.GetDisplayNameOf(Ctrl.FolderItem, SHGDN_FORPARSINGEX | SHGDN_FORPARSING);
+			if (path != "") {
+				Addons.SpeedDial.db.unshift([path, api.GetDisplayNameOf(Ctrl.FolderItem, SHGDN_INFOLDER)].join("\t"));
+				Addons.SpeedDial.db.splice(Addons.SpeedDial.SAVE, MAXINT);
+				Addons.SpeedDial.bSave = true;
+			}
+		}
+	});
+
+	HOME_PATH = Addons.SpeedDial.PATH;
+	try {
+		fso.CreateFolder(Addons.SpeedDial.PATH);
+	} catch (e) {
+	}
+}
