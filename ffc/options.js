984e013f637149e0ea6281828ad23cae4aed0612
Gaku <rxf10242@gmail.com>
2013-12-29 23:14:08 +0900

    13.12.29 Fire File Copy: Added

---
+++ ffc/options.js
@@ -0,0 +1,95 @@
+﻿var arItems = ["Path", "Class", "Copy", "Move"];
+
+function InitOptions()
+{
+	ApplyLang(document);
+
+	var info = GetAddonInfo("ffc");
+	document.title = info.Name;
+	var items = te.Data.Addons.getElementsByTagName("ffc");
+	if (items.length) {
+		var item = items[0];
+		for (i in arItems) {
+			var s = item.getAttribute(arItems[i]);
+			document.F.elements[arItems[i]].value = s ? s : "";
+		}
+	}
+}
+
+function SetOptions()
+{
+	var items = te.Data.Addons.getElementsByTagName("ffc");
+	if (items.length) {
+		var item = items[0];
+		for (i in arItems) {
+			var s = document.F.elements[arItems[i]].value;
+			if (s.length) {
+				item.setAttribute(arItems[i], s);
+			}
+			else {
+				item.removeAttribute(arItems[i]);
+			}
+		}
+		item.setAttribute("Set", 1);
+		TEOk();
+	}
+	window.close();
+}
+
+function SetData(o, s)
+{
+	if (confirmYN(GetText("Are you sure?"))) {
+		document.F.elements[s].value = o.title;
+	}
+}
+
+function Ref()
+{
+	setTimeout(function ()
+	{
+		var commdlg = te.CommonDialog;
+		var arg = api.CommandLineToArgv(document.getElementById("Path").value);
+		commdlg.InitDir = fso.GetParentFolderName(arg.Item(0));
+		commdlg.Filter = "DLL File|*.dll";
+		commdlg.Flags = OFN_FILEMUSTEXIST;
+		if (commdlg.ShowOpen()) {
+			document.getElementById("Path").value = commdlg.FileName;
+		}
+	}, 100);
+}
+
+function RefId(o, s)
+{
+	var dll = api.PathUnquoteSpaces(ExtractMacro(te, document.F.Path.value));
+	var cls = GetAddonOption("ffc", "Class");
+	var item = api.GetModuleFileName(null);
+	var ContextMenu = api.ContextMenu(dll, cls, fso.GetParentFolderName(item), item, HKEY_CLASSES_ROOT, "Folder", null) || api.ContextMenu(dll.replace(/_?64(\.dll)/i, "$1"), cls, fso.GetParentFolderName(item), item, HKEY_CLASSES_ROOT, "Folder", null);
+	if (ContextMenu) {
+		var hMenu = api.CreatePopupMenu();
+		ContextMenu.QueryContextMenu(hMenu, 0, 1, 0x7FFF, CMF_NORMAL);
+		var pt = GetPos(o, true);
+		document.F.elements[s].value = api.TrackPopupMenuEx(hMenu, TPM_RIGHTBUTTON | TPM_RETURNCMD, pt.x, pt.y + o.offsetHeight, te.hwnd, null, ContextMenu);
+		api.DestroyMenu(hMenu);
+	}
+}
+
+function RefClass(o, s)
+{
+	var ar = RegEnumKey(HKEY_CLASSES_ROOT, "Folder\\ShellEx\\DragDropHandlers");
+	var hMenu = api.CreatePopupMenu();
+	try {
+		for (var i = ar.length; i-- > 0;) {
+			if (!ar[i].match(/^{/)) {
+				api.InsertMenu(hMenu, 0, MF_BYPOSITION | MF_STRING, i + 1, ar[i]);
+			}
+		}
+		var pt = GetPos(o, true);
+		var nVerb = api.TrackPopupMenuEx(hMenu, TPM_RIGHTBUTTON | TPM_RETURNCMD, pt.x, pt.y + o.offsetHeight, te.hwnd, null, null);
+		if (nVerb) {
+			document.F.elements[s].value = wsh.RegRead("HKCR\\Folder\\ShellEx\\DragDropHandlers\\" + ar[nVerb - 1] + "\\");
+		}
+	}
+	finally {
+		api.DestroyMenu(hMenu);
+	}
+}
---
+++ ffc/script.js
@@ -0,0 +1,142 @@
+﻿Addon_Id = "ffc";
+
+var items = te.Data.Addons.getElementsByTagName(Addon_Id);
+if (items.length) {
+	var item = items[0];
+	if (!item.getAttribute("Set")) {
+		item.setAttribute("Class", "{E6385E40-E2A6-11d5-ABE6-9EB61339EA35}");
+		item.setAttribute("Copy", 1);
+		item.setAttribute("Move", 2);
+	}
+}
+if (window.Addon == 1) {
+	Addons.FFC =
+	{
+		FO: function (Ctrl, Items, Dest, grfKeyState, pt, pdwEffect, bOver)
+		{
+			var i = Items.Count;
+			if (i == 0 || !(grfKeyState & MK_LBUTTON)) {
+				return false;
+			}
+			var Parent = Items.Item(-1);
+			if (api.ILIsParent(wsh.ExpandEnvironmentStrings("%TEMP%"), Parent, false)) {
+				return false;
+			}
+			if (Dest) {
+				var path = Dest.Path;
+				if (Dest.IsLink) {
+					try {
+						path = Dest.GetLink.Path;
+					} catch (e) {
+						if (e.number == api.LowPart(0x800a0046)) {
+							var sc = wsh.CreateShortcut(Dest.Path);
+							if (sc) {
+								path = sc.TargetPath;
+							}
+						}
+					}
+				}
+				if (path && fso.FolderExists(path)) {
+					while (i--) {
+						if (!IsExists(Items.Item(i).Path)) {
+							pdwEffect.x = DROPEFFECT_NONE;
+							break;
+						}
+					}
+					if (pdwEffect.x) {
+						nVerb = -1;
+						if (bOver) {
+							var DropTarget = api.DropTarget(path);
+							DropTarget.DragOver(Items, grfKeyState, pt, pdwEffect);
+						}
+						if (pdwEffect.x & DROPEFFECT_COPY) {
+							nVerb = GetAddonOption("ffc", "Copy");
+						}
+						else if (pdwEffect.x & DROPEFFECT_MOVE) {
+							nVerb = GetAddonOption("ffc", "Move");
+						}
+						nVerb = api.LowPart(nVerb);
+						if (nVerb > 0) {
+							var dll = api.PathUnquoteSpaces(ExtractMacro(te, GetAddonOption("ffc", "Path") + ""));
+							var cls = GetAddonOption("ffc", "Class");
+							var ContextMenu = api.ContextMenu(dll, cls, path, Items, HKEY_CLASSES_ROOT, "Folder", null) || api.ContextMenu(dll.replace(/_?64(\.dll)/i, "$1"), cls, path, Items, HKEY_CLASSES_ROOT, "Folder", null);
+							if (ContextMenu) {
+								var hMenu = api.CreatePopupMenu();
+								ContextMenu.QueryContextMenu(hMenu, 0, 1, 0x7FFF, CMF_NORMAL);
+								var hr = ContextMenu.InvokeCommand(0, te.hwnd, nVerb - 1, null, null, SW_SHOWNORMAL, 0, 0);
+								api.DestroyMenu(hMenu);
+								if (hr >= 0) {
+									return true;
+								}
+							}
+						}
+					}
+				}
+			}
+			return false;
+		}
+	};
+
+	AddEvent("Drop", function (Ctrl, dataObj, grfKeyState, pt, pdwEffect)
+	{
+		switch (Ctrl.Type) {
+			case CTRL_SB:
+			case CTRL_EB:
+				var Items = Ctrl.Items();
+				var Dest;
+				var i = Ctrl.HitTest(pt, LVHT_ONITEM);
+				if (i >= 0) {
+					Dest = Items.Item(i);
+					if (!fso.FolderExists(Dest.Path)) {
+						if (api.DropTarget(Dest)) {
+							return E_FAIL;
+						}
+						Dest = Ctrl.FolderItem;
+					}
+				}
+				else {
+					Dest = Ctrl.FolderItem;
+				}
+				if (Addons.FFC.FO(Ctrl, dataObj, Dest, grfKeyState, pt, pdwEffect, true)) {
+					return S_OK
+				}
+				break;
+			case CTRL_DT:
+				if (Addons.FFC.FO(null, dataObj, Ctrl.FolderItem, grfKeyState, pt, pdwEffect, true)) {
+					return S_OK
+				}
+				break;
+		}
+	});
+
+	AddEvent("Command", function (Ctrl, hwnd, msg, wParam, lParam)
+	{
+		if (Ctrl.Type == CTRL_SB || Ctrl.Type == CTRL_EB) {
+			switch ((wParam & 0xfff) + 1) {
+				case CommandID_PASTE:
+					var Items = api.OleGetClipboard()
+					if (Addons.FFC.FO(null, Items, Ctrl.FolderItem, MK_LBUTTON, null, Items.pdwEffect, false)) {
+						return S_OK;
+					}
+					break;
+			}
+		}
+	});
+
+	AddEvent("InvokeCommand", function (ContextMenu, fMask, hwnd, Verb, Parameters, Directory, nShow, dwHotKey, hIcon)
+	{
+		switch (Verb + 1) {
+			case CommandID_PASTE:
+				var Target = ContextMenu.Items();
+				if (Target.Count) {
+					var Items = api.OleGetClipboard()
+					if (Addons.FFC.FO(null, Items, Target.Item(0), MK_LBUTTON, null, Items.pdwEffect, false)) {
+						return S_OK;
+					}
+				}
+				break;
+		}
+	});
+	te.HookDragDrop(CTRL_FV, true);
+	te.HookDragDrop(CTRL_TV, true);
+}
