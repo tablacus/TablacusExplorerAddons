9b3bfbdf89ed6d3bd1f1e86369b7392c148e7578
Gaku <rxf10242@gmail.com>
2014-03-03 23:35:23 +0900

    14.3.3

---
+++ preventduplicatetabs/script.js
@@ -0,0 +1,19 @@
+﻿if (window.Addon == 1) {
+	AddEvent("BeforeNavigate", function (Ctrl, fs, wFlags, Prev)
+	{
+		var TC = Ctrl.Parent;
+		for (var i = TC.Count; i-- > 0;) {
+			var Item = TC.Item(i);
+			if (Ctrl.hwnd != Item.hwnd && api.ILIsEqual(Ctrl.FolderItem, Item.FolderItem)) {
+				if (!(wFlags & SBSP_ACTIVATE_NOFOCUS) || TC.Selected.hwnd == Ctrl.hwnd) {
+					TC.SelectedIndex = i;
+				}
+				return E_FAIL;
+			}
+		}
+	});
+
+	if (!HOME_PATH) {
+		HOME_PATH = ssfRECENT;
+	}
+}
--- speeddial/script.js
+++ speeddial/script.js
@@ -3,16 +3,40 @@
 	{
 		SAVE: 512,
 		DISP: 10,
-		PATH: fso.BuildPath(te.Data.DataFolder, "newtab"),
+		PATH: "about:newtab",
 		CONFIG: fso.BuildPath(te.Data.DataFolder, "config\\speeddial.tsv"),
 		db: [],
-		bSave: false
+		bSave: false,
+		Prev: null,
+		
+		IsHandle: function (Ctrl)
+		{
+			return api.PathMatchSpec(Ctrl.FolderItem.Path, Addons.SpeedDial.PATH);
+		},
+		
+		IsDisp: function (path)
+		{
+			if (!api.PathMatchSpec("\\\\")) {
+				if (api.PathMatchSpec(path, "?:\\*")) {
+					try {
+						var d = fso.GetDrive(path[0]);
+						return d.DriveType == 2;
+					}
+					catch (e) {
+						return false;
+					}
+				}
+				return true;
+			}
+			return false;
+		}
+
 	}
 	try {
 		var f = fso.OpenTextFile(Addons.SpeedDial.CONFIG, 1, false, -1);
 		var s;
 		while (s = f.ReadLine()) {
-			Addons.SpeedDial.db.push(s);
+			Addons.SpeedDial.db.push(s.replace(/\t.*$/, ""));
 		}
 		f.Close();
 	}
@@ -26,7 +50,10 @@
 			try {
 				var f = fso.OpenTextFile(Addons.SpeedDial.CONFIG, 2, true, -1);
 				for (var i in Addons.SpeedDial.db) {
-					f.WriteLine(Addons.SpeedDial.db[i]);
+					var path = Addons.SpeedDial.db[i];
+					if (Addons.SpeedDial.IsDisp(path)) {
+						f.WriteLine(path);
+					}
 				}
 				f.Close();
 			}
@@ -36,43 +63,84 @@
 
 	AddEvent("ListViewCreated", function (Ctrl)
 	{
-		if (api.ILIsEqual(Ctrl.FolderItem, Addons.SpeedDial.PATH)) {
-			api.SHFileOperation(FO_DELETE, fso.BuildPath(Addons.SpeedDial.PATH, "*.lnk"), null, FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_SILENT, false);
-			var hash = {};
-			for (var i in Addons.SpeedDial.db) {
-				hash[Addons.SpeedDial.db[i]] = api.QuadPart(hash[Addons.SpeedDial.db[i]]) + 1;
-			}
-			var keys = [];
-			for (var i in hash) {
-				keys.push(i);
-			}
-			keys.sort(function (a, b) {
-				return hash[b] - hash[a];
-			});
-			keys.splice(Addons.SpeedDial.DISP, MAXINT);
-			for (var i = 0; i < keys.length; i ++) {
-				try {
-					var ar = keys[i].split("\t");
-					var sc = wsh.CreateShortcut(fso.BuildPath(Addons.SpeedDial.PATH, api.sprintf(9, "%02d_", i + 1) + ar[1].replace(/:/g, "") + ".lnk"));
-					sc.TargetPath = ar[0] == "0" ? "Shell:Desktop" : ar[0];
-					sc.Save();
-				} catch (e) {
+		if (Addons.SpeedDial.IsHandle(Ctrl)) {
+			setTimeout(function () {
+				var keys = [];
+				var hash = {};
+				for (var i in Addons.SpeedDial.db) {
+					var path = Addons.SpeedDial.db[i];
+					if (hash[path]) {
+						hash[path]++;
+					}
+					else {
+						hash[path] = 1;
+					}
 				}
-			}
+				for (var i in hash) {
+					keys.push(i);
+				}
+				keys.sort(function (a, b) {
+					return hash[b] - hash[a];
+				});
+				var Items = Ctrl.Items();
+				for (var i = Items.Count; i--;) {
+					Ctrl.RemoveItem(Items.Item(i));
+				}
+				var nDog = Addons.SpeedDial.DISP;
+				for (var i = 0; i < keys.length; i++) {
+					if (Addons.SpeedDial.IsDisp(keys[i])) {
+						Ctrl.AddItem(keys[i]);
+						if (!--nDog) {
+							break;
+						}
+					}
+				}
+			}, 100);
 		}
 		else {
-			var path = api.GetDisplayNameOf(Ctrl.FolderItem, SHGDN_FORPARSINGEX | SHGDN_FORPARSING);
-			if (path != "") {
-				Addons.SpeedDial.db.unshift([path, api.GetDisplayNameOf(Ctrl.FolderItem, SHGDN_INFOLDER)].join("\t"));
+			var path = api.GetDisplayNameOf(Ctrl.FolderItem, SHGDN_FORPARSINGEX | SHGDN_FORPARSING | SHGDN_FORADDRESSBAR);
+			if (path != "" && IsSavePath(path) && Addons.SpeedDial.IsDisp(path)) {
+				Addons.SpeedDial.db.unshift(path);
 				Addons.SpeedDial.db.splice(Addons.SpeedDial.SAVE, MAXINT);
 				Addons.SpeedDial.bSave = true;
 			}
 		}
 	});
 
+	AddEvent("TranslatePath", function (Ctrl, Path)
+	{
+		if (api.PathMatchSpec(Path, Addons.SpeedDial.PATH)) {
+			return ssfRESULTSFOLDER;
+		}
+	}, true);
+
+	AddEvent("GetTabName", function (Ctrl)
+	{
+		if (Addons.SpeedDial.IsHandle(Ctrl)) {
+			return GetText("New Tab");
+		}
+	}, true);
+
+	AddEvent("Command", function (Ctrl, hwnd, msg, wParam, lParam)
+	{
+		if (Ctrl.Type == CTRL_SB || Ctrl.Type == CTRL_EB) {
+			if (Addons.SpeedDial.IsHandle(Ctrl)) {
+				if ((wParam & 0xfff) == CommandID_DELETE - 1) {
+					return S_OK;
+				}
+			}
+		}
+	});
+
+	AddEvent("InvokeCommand", function (ContextMenu, fMask, hwnd, Verb, Parameters, Directory, nShow, dwHotKey, hIcon)
+	{
+		if (Verb == CommandID_DELETE - 1) {
+			var FV = ContextMenu.FolderView;
+			if (FV && Addons.SpeedDial.IsHandle(FV)) {
+				return S_OK;
+			}
+		}
+	});
+
 	HOME_PATH = Addons.SpeedDial.PATH;
-	try {
-		fso.CreateFolder(Addons.SpeedDial.PATH);
-	} catch (e) {
-	}
 }
