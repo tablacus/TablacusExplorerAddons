cc1aed0328dc7ad1b0ee7fb7969796ddb92203c1
Gaku <rxf10242@gmail.com>
2015-01-04 23:57:20 +0900

    15.1.4 Added: Replace slow columns

---
+++ replaceslowcolumns/script.js
@@ -0,0 +1,16 @@
+﻿if (window.Addon == 1) {
+	Addons.ReplaceSlowColumns =
+	{
+		ItemDate: '"' + api.PSGetDisplayName("{F7DB74B4-4287-4103-AFBA-F1B13DCD75CF} 100") + '"',
+		DateModified: '"' + api.PSGetDisplayName("{B725F130-47EF-101A-A5F1-02608C9EEBAC} 14") + '"'
+	};
+	if (Addons.ReplaceSlowColumns.ItemDate != '""') {
+		AddEvent("NavigateComplete", function (Ctrl)
+		{
+			var s = Ctrl.Columns;
+			if (s.match(Addons.ReplaceSlowColumns.ItemDate)) {
+				Ctrl.Columns = s.replace(Addons.ReplaceSlowColumns.ItemDate, Addons.ReplaceSlowColumns.DateModified);
+			}
+		});
+	}
+}
--- startup/script.js
+++ startup/script.js
@@ -1,14 +1,14 @@
 var Addon_Id = "startup";
 
 if (window.Addon == 1) {
-	if (xmlWindow) {
-		AddEventEx(window, "load", function ()
-		{
+	AddEvent("Create", function (Ctrl)
+	{
+		if (Ctrl.Type == CTRL_TE) {
 			var items = te.Data.Addons.getElementsByTagName(Addon_Id);
 			if (items.length) {
 				var item = items[0];
 				Exec(te, item.getAttribute("Path"), item.getAttribute("Type"), te.hwnd);
 			}
-		});
-	}
+		}
+	});
 }
