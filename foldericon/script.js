76aca74ac4a175197b7e5f337e8ae54fd0986b2f
Gaku <rxf10242@gmail.com>
2015-01-30 21:24:58 +0900

    15.1.30

--- foldericon/script.js
+++ foldericon/script.js
@@ -1,13 +1,8 @@
-﻿if (Addon == 1) {
-	g_foldericon_ChangeView = window.ChangeView;
-	window.ChangeView = function (Ctrl)
+﻿if (window.Addon == 1) {
+	AddEvent("ChangeView", function (Ctrl)
 	{
 		var info = api.Memory("SHFILEINFO");
-		api.ShGetFileInfo(Ctrl.FolderItem, 0, info, info.Size, SHGFI_ICON | SHGFI_SMALLICON | SHGFI_OPENICON | SHGFI_PIDL);
-		api.SendMessage(external.hwnd, WM_SETICON, ICON_SMALL, info.hIcon);
-		if (g_foldericon_ChangeView) {
-			return g_foldericon_ChangeView(Ctrl);
-		}
-		return S_OK;
-	}
+		api.SHGetFileInfo(Ctrl.FolderItem, 0, info, info.Size, SHGFI_ICON | SHGFI_SMALLICON | SHGFI_OPENICON | SHGFI_PIDL);
+		api.SendMessage(te.hwnd, WM_SETICON, ICON_SMALL, info.hIcon);
+	});
 }
--- openinstead/script.js
+++ openinstead/script.js
@@ -3,31 +3,35 @@ if (window.Addon == 1) {
 	{
 		Exec: function ()
 		{
-			var sw = sha.Windows();
-			for (var i = sw.Count; i-- > 0;) {
-				var exp = sw.item(i);
-				if (exp && exp.Visible && !exp.Busy) {
-					var doc = exp.Document;
-					if (doc) {
-						if (Addons.OpenInstead.Match(api.GetDisplayNameOf(doc, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING)) == S_OK) {
-							exp.Visible = false;
-							var FV = te.Ctrl(CTRL_FV);
-							FV = FV.Navigate(doc, SBSP_NEWBROWSER);
-							if (Addons.OpenInstead.TakeOver) {
-								FV.CurrentViewMode = doc.CurrentViewMode;
-								if (doc.IconSize) {
-									FV.IconSize = doc.IconSize;
+			try {
+				var sw = sha.Windows();
+				for (var i = sw.Count; i-- > 0;) {
+					var exp = sw.item(i);
+					if (exp && exp.Visible && !exp.Busy) {
+						var doc = exp.Document;
+						if (doc) {
+							if (Addons.OpenInstead.Match(api.GetDisplayNameOf(doc, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING)) == S_OK) {
+								exp.Visible = false;
+								var FV = te.Ctrl(CTRL_FV);
+								FV = FV.Navigate(doc, SBSP_NEWBROWSER);
+								if (Addons.OpenInstead.TakeOver) {
+									FV.CurrentViewMode = doc.CurrentViewMode;
+									if (doc.IconSize) {
+										FV.IconSize = doc.IconSize;
+									}
+									if (doc.SortColumns) {
+										FV.SortColumns = doc.SortColumns;
+									}
 								}
-								if (doc.SortColumns) {
-									FV.SortColumns = doc.SortColumns;
-								}
+								exp.Quit();
+								RestoreFromTray();
+								api.SetForegroundWindow(te.hwnd);
 							}
-							exp.Quit();
-							RestoreFromTray();
-							api.SetForegroundWindow(te.hwnd);
 						}
 					}
 				}
+			}
+			catch (e) {
 			}
 		},
 
