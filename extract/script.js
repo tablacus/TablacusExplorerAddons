ec661809a0e2eb0fb9e79660690cc625633b62c0
Gaku <rxf10242@gmail.com>
2015-02-14 22:56:38 +0900

    15.2.14

--- extract/script.js
+++ extract/script.js
@@ -1,3 +1,5 @@
+var Addon_Id = 'extract';
+
 if (window.Addon == 1) {
 	AddEvent("Extract", function (Src, Dest)
 	{
@@ -10,4 +12,28 @@ if (window.Addon == 1) {
 			}
 		}
 	});
+}
+else {
+	var s = [];
+	var path = 'C:\\Program Files\\7-Zip\\7zG.exe';
+	var path2 = 'C:\\Program Files (x86)\\7-Zip\\7zG.exe';
+	if (!fso.FileExists(path) && fso.FileExists(path2)) {
+		path = path2;
+	}
+	document.getElementById("_7zip").innerHTML = ['<input type="button" title=\'', api.PathQuoteSpaces(path), ' x %src% -o%dest%\' value="7-Zip" onclick="SetExtract(this)"> '].join("");
+
+	path = "C:\\Program Files\\Lhaz\\Lhaz.exe";
+	path2 = "C:\\Program Files (x86)\\Lhaz\\Lhaz.exe";
+	if (!fso.FileExists(path) && fso.FileExists(path2)) {
+		path = path2;
+	}
+	document.getElementById("_lhaz").innerHTML = ['<input type="button" title=\'', api.PathQuoteSpaces(path), ' /e /d%dest% %src%\' value="Lhaz" onclick="SetExtract(this)"> '].join("");
+
+	SetExtract = function (o)
+	{
+		if (confirmOk(GetText("Are you sure?"))) {
+			document.F.Path.value = o.title;
+		}
+	}
+	InitAddonOptions();
 }
