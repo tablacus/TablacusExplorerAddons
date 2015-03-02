b3ddac0032ae954078d7496348cc5eb4dac9beda
Gaku <rxf10242@gmail.com>
2015-03-03 00:17:40 +0900

    15.3.2 Added: Debug

---
+++ debug/script.js
@@ -0,0 +1,22 @@
+﻿var Addon_Id = "debug";
+var Default = "BottomBar2Left";
+
+if (window.Addon == 1) {
+	Addons.Debug =
+	{
+		alert: function (s)
+		{
+			var o = document.getElementById("debug");
+			o.value += "\n" + s;
+			o.scrollTop = o.scrollHeight;
+		},
+		clear: function ()
+		{
+			var o = document.getElementById("debug");
+			o.value = "Ready.";
+		}
+	};
+	SetAddon(Addon_Id, Default, '<textarea id="debug" style="width: 100%; height: 100px">Ready.</textarea>');
+
+	AddEventEx(window, "beforeunload", Addons.Debug.clear);
+}
