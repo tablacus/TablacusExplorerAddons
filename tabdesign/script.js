1b265216e8a40eadce9dc8364f4b99e97148452d
Gaku <rxf10242@gmail.com>
2015-03-15 22:17:41 +0900

    15.3.15

--- tabdesign/script.js
+++ tabdesign/script.js
@@ -10,11 +10,11 @@ if (window.Addon == 1) {
 	newStyle.type = "text/css";
 	document.getElementsByTagName("head").item(0).appendChild(newStyle);
 	var css = document.styleSheets.item(0);
-	var sd = "border-radius: 9px 9px 0px 0px";
 	var ar = ["activetab", "tab", "tab2"];
+	var sd = ["border-radius: 9px 9px 0px 0px; border-bottom: 0px", "border-radius: 9px 9px 0px 0px", "border-radius: 9px 9px 0px 0px"];
 	for (var i in ar) {
 		if (css.insertRule) {
-			css.insertRule([".", ar[i], " { ", item.getAttribute(ar[i]) || sd, " }"].join(""), css.cssRules.length);
+			css.insertRule([".", ar[i], " { ", item.getAttribute(ar[i]) || sd[i], " }"].join(""), css.cssRules.length);
 		}
 		else if (css.addRule) {
 			css.addRule("." + ar[i], item.getAttribute(ar[i]) || sd);
