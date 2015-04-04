a058812b041c418670d49da2adec1cf5fc5b7344
Gaku <rxf10242@gmail.com>
2015-04-04 22:21:09 +0900

    15.4.4 Added: Disable Classic Style during Drag

---
+++ disableclassicdrag/script.js
@@ -0,0 +1,17 @@
+﻿if (window.Addon == 1) {
+	AddEvent("DragEnter", function (Ctrl, dataObj, grfKeyState, pt, pdwEffect)
+	{
+		if (Ctrl.Type <= CTRL_EB) {
+			api.SetWindowTheme(Ctrl.hwndList, "explorer", null);
+		}
+	});
+
+	AddEvent("DragLeave", function (Ctrl, dataObj, grfKeyState, pt, pdwEffect)
+	{
+		if (Ctrl.Type <= CTRL_EB && Addons.ClassicStyle) {
+			api.SetWindowTheme(Ctrl.hwndList, null, null);
+		}
+	});
+
+	te.HookDragDrop(CTRL_FV, true);
+}
