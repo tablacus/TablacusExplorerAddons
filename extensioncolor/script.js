9e49763d39e89ce286a816cc58009b500bba4284
Gaku <rxf10242@gmail.com>
2015-05-12 22:12:04 +0900

    15.5.12

---
+++ extensioncolor/script.js
@@ -0,0 +1,32 @@
+﻿if (window.Addon == 1) {
+	Addons.ExtensionColor = {
+		Color: {}
+	};
+	var re = /^\/(.*)\/(.*)/;
+	try {
+		var ado = te.CreateObject("Adodb.Stream");
+		ado.CharSet = "utf-8";
+		ado.Open();
+		ado.LoadFromFile(fso.BuildPath(te.Data.DataFolder, "config\\extensioncolor.tsv"));
+		while (!ado.EOS) {
+			var ar = ado.ReadText(adReadLine).split("\t");
+			if (ar[0] != "") {
+				Addons.ExtensionColor.Color[ar[0]] = GetWinColor(ar[1]);
+			}
+		}
+		ado.Close();
+	}
+	catch (e) {
+	}
+
+	AddEvent("ItemPrePaint", function (Ctrl, pid, nmcd, vcd, plRes)
+	{
+		if (pid) {
+			var c = Addons.ExtensionColor.Color[fso.GetExtensionName(api.GetDisplayNameOf(pid, SHGDN_FORPARSING))];
+			if (isFinite(c)) {
+				vcd.clrText = c;
+				return S_OK;
+			}
+		}
+	});
+}
