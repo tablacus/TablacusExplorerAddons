86adbbe25066d66db005c892ec41d4575e5d915f
Gaku <rxf10242@gmail.com>
2015-07-09 23:20:04 +0900

    15.7.9 Added: Firebug Lite

---
+++ firebug/script.js
@@ -0,0 +1,35 @@
+﻿if (window.Addon == 1) {
+	Addons.Firebug =
+	{
+		Resize:  function ()
+		{
+			Addons.Firebug.div.style.height = Addons.Firebug.fbChrome.offsetHeight + "px";
+			Resize();
+		},
+
+		Init: function ()
+		{
+			if (window.Firebug) {
+				Firebug.extend(function(FBL)
+				{
+					Addons.Firebug.fbChrome = FBL.$("fbChrome");
+					Addons.Firebug.fbChrome.onmouseup = Addons.Firebug.Resize;
+					Addons.Firebug.Resize();
+				});
+			}
+			else {
+				setTimeout(Addons.Firebug.Init, 1000);
+			}
+		}
+
+	};
+
+	var firebug = document.createElement('script');
+	firebug.setAttribute('src', 'https://getfirebug.com/firebug-lite.js#overrideConsole=true,startOpened=true,enableTrace=true');
+	document.body.appendChild(firebug);
+
+	Addons.Firebug.div = document.createElement('div');
+	document.getElementById("bottombar").appendChild(Addons.Firebug.div);
+
+	setTimeout(Addons.Firebug.Init, 1000);
+}
