6a76ac312e5c9e3b8a0abdc563c390328aa572ca
Gaku <rxf10242@gmail.com>
2016-11-05 22:28:13 +0900

    16.11.5 Added: Select tab

--- preventduplicatetabs/script.js
+++ preventduplicatetabs/script.js
@@ -7,10 +7,14 @@
 			var Item = TC.Item(i);
 			if (Ctrl.hwnd != Item.hwnd && api.ILIsEqual(Ctrl.FolderItem, Item.FolderItem)) {
 				if (!(wFlags & SBSP_ACTIVATE_NOFOCUS) || TC.Selected.hwnd == Ctrl.hwnd) {
-					(function (TC, FV, SelectedIndex) { setTimeout(function () {
-						TC.SelectedIndex = SelectedIndex;
-						FV.Close();
-					}, 99);}) (TC, Ctrl, i);
+					(function (TC, cFV) { setTimeout(function () {
+						for (var i = 2; i--;) {
+							if (cFV[i].Close()) {
+								TC.SelectedIndex = cFV[i ^ 1].Index;
+								break;
+							}
+						}
+					}, 99);}) (TC, [Ctrl, Item]);
 					continue;
 				}
 				return E_FAIL;
@@ -27,7 +31,6 @@
 			var SelectedIndex = TC.SelectedIndex;
 			for (var i in db) {
 				var cFV = db[i];
-				var n;
 				var bSelected = false;
 				for (var j = cFV.length; cFV.length > 1 && j-- > 0;) {
 					var FV = cFV[j];
