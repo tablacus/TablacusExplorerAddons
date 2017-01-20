e39107aab1dc27ccd77617aaadb10f69fef28f3d
Gaku <rxf10242@gmail.com>
2017-01-21 00:02:28 +0900

    Added: Colored with access date

---
+++ accessdatecolor/options.js
@@ -0,0 +1,128 @@
+SetTabContents(4, "Color",'<form name="E" id="data1"><table id="T" style="width: 100%"></table></form>');
+document.getElementById("toolbar").innerHTML = '<input type="button" value="Add" onclick=\'Add(["",""])\' />&emsp;<input type="button" value="Up" onclick="Up()" /><input type="button" value="Down" onclick="Down()" />&emsp;<input type="button" value="Remove" onclick="Remove()" />';
+
+Get = function (i)
+{
+	return [document.E.elements['p' + i].value, document.E.elements['c' + i].value];
+}
+
+Set = function (i, ar)
+{
+	document.E.elements['p' + i].value = ar[0];
+	document.E.elements['c' + i].value = ar[1];
+	document.E.elements['b' + i].style.backgroundColor = ar[1];
+}
+
+Add = function(ar)
+{
+	var table = document.getElementById("T");
+	var nRows = table.rows.length;
+	s = ['<td style="width: 1em"><input type="radio" name="sel" id="i', nRows, '" /></td>'];
+	s.push('<td><input type="text" name="p', nRows, '" style="width: 100%" onchange="FilterChanged(this)" placeholder="time 1s 1m 1h 1d 1w 1y" title="time 1s 1m 1h 1d 1w 1y" /></td>');
+	var cl = GetText("Color");
+	s.push('<td style="width: 7em"><input type="text" name="c', nRows, '" style="width: 100%" placeholder="', cl, '" title="', cl, '" onchange="FilterChanged()"  /></td>');
+	s.push('<td style="width: 1em"><input type="button" name="b', nRows, '" value=" " class="color" style="width: 100%" onclick="ChooseColor2(this)" title="', cl, '" /></td>');
+	var tr = table.insertRow();
+	tr.innerHTML = s.join("");
+	Set(nRows, ar);
+	return tr;
+}
+
+
+function GetIndex()
+{
+	var table = document.getElementById("T");
+	for (var i = table.rows.length; i--;) {
+		if (document.getElementById("i" + i).checked) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+Up = function ()
+{
+	var nPos = GetIndex();
+	if (nPos <= 0) {
+		return;
+	}
+	var s = Get(nPos);
+	Set(nPos, Get(nPos - 1));
+	Set(--nPos, s);
+	document.E.elements["i" + nPos].checked = true;
+}
+
+Down = function ()
+{
+	var table = document.getElementById("T");
+	var nPos = GetIndex();
+	if (nPos < 0 || nPos >= table.rows.length - 1) {
+		return;
+	}
+	var s = Get(nPos);
+	Set(nPos, Get(nPos + 1));
+	Set(++nPos, s);
+	document.E.elements["i" + nPos].checked = true;
+}
+
+Remove = function ()
+{
+	var nPos = GetIndex();
+	if (nPos < 0 || !confirmOk("Are you sure?")) {
+		return;
+	}
+	var table = document.getElementById("T");
+	var nRows = table.rows.length;
+	for (var i = nPos; i < nRows - 1; i++) {
+		Set(i, Get(i + 1));
+	}
+	table.deleteRow(nRows - 1);
+}
+
+FilterChanged = function(o)
+{
+	g_bChanged = true;
+}
+
+ChangeColor = function(o)
+{
+	var n = o.name.replace(/\D/, "");
+	document.E.elements["b" + n].style.backgroundColor = o.value;
+	g_bChanged = true;
+}
+
+ChooseColor2 = function(o)
+{
+	var n = o.name.replace(/\D/, "");
+	var oc = document.E.elements["c" + n];
+	var c = ChooseWebColor(oc.value);
+	if (c) {
+		oc.value = c;
+		ChangeColor(oc);
+	}
+}
+
+SaveLocation = function ()
+{
+	try {
+		var ado = new ActiveXObject(api.ADBSTRM);
+		ado.CharSet = "utf-8";
+		ado.Open();
+		var table = document.getElementById("T");
+		var nRows = table.rows.length;
+		for (var i = 0; i < nRows; i++) {
+			ado.WriteText([document.E.elements['p' + i].value, document.E.elements['c' + i].value].join("\t") + "\r\n");
+		}
+		ado.SaveToFile(fso.BuildPath(te.Data.DataFolder, "config\\accessdatecolor.tsv"), adSaveCreateOverWrite);
+		ado.Close();
+	} catch (e) {}
+}
+
+try {
+	var ado = OpenAdodbFromTextFile(fso.BuildPath(te.Data.DataFolder, "config\\accessdatecolor.tsv"));
+	while (!ado.EOS) {
+		var ar = ado.ReadText(adReadLine).split("\t");
+		Add(ar);
+	}
+	ado.Close();
+} catch (e) {}
---
+++ accessdatecolor/script.js
@@ -0,0 +1,94 @@
+﻿var Addon_Id = "accessdatecolor";
+var Default = "None";
+
+var item = GetAddonElement(Addon_Id);
+if (window.Addon == 1) {
+	Addons.AccessDateColor = {
+		Color: [],
+		nPos: 0,
+		strName: "",
+		Enabled: true,
+
+		Exec: function ()
+		{
+			Addons.AccessDateColor.Enabled = !Addons.AccessDateColor.Enabled;
+			api.RedrawWindow(te.hwnd, null, 0, RDW_NOERASE | RDW_INVALIDATE | RDW_ALLCHILDREN);
+			return S_OK;
+		},
+
+		Popup: function ()
+		{
+			return false;
+		}
+	};
+	try {
+		var smhdw = {s: 1000, m: 60000,  h:3600000, d: 86400000, w: 604800000, y: 31536000000 };
+		var ado = OpenAdodbFromTextFile(fso.BuildPath(te.Data.DataFolder, "config\\accessdatecolor.tsv"));
+		var tzo = new Date().getTimezoneOffset() * 60000;
+		while (!ado.EOS) {
+			var ar = ado.ReadText(adReadLine).split("\t");
+			if (ar[0]) {
+				s = (ar[0].replace(/([\dx]+)([smhdw])/ig, function (all, re1, re2)
+				{
+					return eval(re1.replace(/x/ig, "*")) * smhdw[re2.toLowerCase()] + '+';
+				}).replace(/\+$/, "")) - tzo;
+				Addons.AccessDateColor.Color.push([s, ar[1] ? GetWinColor(ar[1]) : -1]);
+			}
+		}
+		ado.Close();
+	} catch (e) {}
+	Addons.AccessDateColor.Color = Addons.AccessDateColor.Color.sort(function (a, b)
+	{
+		return b[0] - a[0];
+	});
+	AddEvent("ItemPrePaint", function (Ctrl, pid, nmcd, vcd, plRes)
+	{
+		if (pid && Addons.AccessDateColor.Enabled) {
+			var d = new Date() - pid.ExtendedProperty("Access");
+			for (var i = Addons.AccessDateColor.Color.length; i--;) {
+				var ar = Addons.AccessDateColor.Color[i];
+				if (d < ar[0]) {
+					if (ar[1] != -1) {
+						vcd.clrText = ar[1];
+						return S_OK;
+					} else {
+						return;
+					}
+				}
+			}
+		}
+	});
+
+	Addons.AccessDateColor.strName = item.getAttribute("MenuName") || GetText(GetAddonInfo(Addon_Id).Name);
+	//Menu
+	if (item.getAttribute("MenuExec")) {
+		Addons.AccessDateColor.nPos = api.LowPart(item.getAttribute("MenuPos"));
+		AddEvent(item.getAttribute("Menu"), function (Ctrl, hMenu, nPos)
+		{
+			api.InsertMenu(hMenu, Addons.AccessDateColor.nPos, MF_BYPOSITION | MF_STRING | Addons.AccessDateColor.Enabled ? MF_CHECKED : 0, ++nPos, GetText(Addons.AccessDateColor.strName));
+			ExtraMenuCommand[nPos] = Addons.AccessDateColor.Exec;
+			return nPos;
+		});
+	}
+	//Key
+	if (item.getAttribute("KeyExec")) {
+		SetKeyExec(item.getAttribute("KeyOn"), item.getAttribute("Key"), Addons.AccessDateColor.Exec, "Func");
+	}
+	//Mouse
+	if (item.getAttribute("MouseExec")) {
+		SetGestureExec(item.getAttribute("MouseOn"), item.getAttribute("Mouse"), Addons.AccessDateColor.Exec, "Func");
+	}
+	//Type
+	AddTypeEx("Add-ons", "Access date color", Addons.AccessDateColor.Exec);
+
+	var h = GetAddonOption(Addon_Id, "IconSize") || window.IconSize || 24;
+	var s = GetAddonOption(Addon_Id, "Icon");
+	if (s) {
+		s = '<img title="' + Addons.AccessDateColor.strName + '" src="' + s.replace(/"/g, "") + '" width="' + h + 'px" height="' + h + 'px" />';
+	} else {
+		s = Addons.AccessDateColor.strName;
+	}
+	SetAddon(Addon_Id, Default, ['<span class="button" onclick="Addons.AccessDateColor.Exec();" oncontextmenu="Addons.AccessDateColor.Popup(); return false;" onmouseover="MouseOver(this)" onmouseout="MouseOut()">', s, '</span>']);
+} else {
+	importScript("addons\\" + Addon_Id + "\\options.js");
+}
