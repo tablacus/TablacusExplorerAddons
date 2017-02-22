7e8d40617193b6312b378674fdb974ed4dce8142
Gaku <rxf10242@gmail.com>
2017-02-22 22:24:38 +0900

    17.2.22

--- tooltippreview/script.js
+++ tooltippreview/script.js
@@ -6,10 +6,9 @@ if (window.Addon == 1) {
 		if (Ctrl.Type <= CTRL_EB && Index >= 0) {
 			var Item = Ctrl.Items.Item(Index);
 			if (Item) {
-				var image = te.GdiplusBitmap();
-				image.FromFile(Item.Path);
-				var x = image.GetWidth();
-				if (x) {
+				var image = te.WICBitmap().FromFile(Item.Path);
+				if (image) {
+					var x = image.GetWidth();
 					var y = image.GetHeight();
 					if (x > y) {
 						y = Addons.TooltipPreview.MAX * y / x;
@@ -37,14 +36,15 @@ if (window.Addon == 1) {
 										if (x > 48 || y > 48) {
 											var hdc = api.GetWindowDC(hwnd1);
 											if (hdc) {
-												var hbm = thum.GetHBITMAP();
+												var hbm = thum.GetHBITMAP(GetSysColor(COLOR_WINDOW));
 												if (hbm) {
 													var hmdc = api.CreateCompatibleDC(hdc);
 													api.SelectObject(hmdc, hbm);
 													var rc = api.Memory("RECT");
 													api.GetClientRect(hwnd1, rc);
-													var x1 = Math.floor((rc.Right - rc.Left - x) / 2);
-													var y1 = Math.floor((rc.Bottom - rc.Top - y) / 2);
+													var x1 = Math.floor((rc.Right-- - rc.Left++ - x) / 2);
+													var y1 = Math.floor((rc.Bottom-- - rc.Top++ - y) / 2);
+													api.FillRect(hdc, rc, null);
 													api.BitBlt(hdc, x1, y1, x, y, hmdc, 0, 0, SRCCOPY);
 													if (x1 < 0 && Addons.TooltipPreview.SIZE.cx > 1) {
 														Addons.TooltipPreview.SIZE.cx--;
@@ -75,26 +75,22 @@ if (window.Addon == 1) {
 							}
 						}
 					}, 200);
-					if (x > 48 || y > 48) {
-						var s = "";
-						if (Addons.TooltipPreview.SIZE.cx == 0) {
-							var hdc = api.GetWindowDC(te.hwnd);
-							if (hdc) {
-								api.GetTextExtentPoint32(hdc, " ", Addons.TooltipPreview.SIZE);
-								api.ReleaseDC(te.hwnd, hdc);
-							}
+					var s = "";
+					if (Addons.TooltipPreview.SIZE.cx == 0) {
+						var hdc = api.GetWindowDC(te.hwnd);
+						if (hdc) {
+							api.GetTextExtentPoint32(hdc, " ", Addons.TooltipPreview.SIZE);
+							api.ReleaseDC(te.hwnd, hdc);
 						}
-						var ar = new Array(Math.floor(y / Addons.TooltipPreview.SIZE.cy));
-						if (x > 96) {
-							var col = ["name", "type", "write", "{6444048F-4C8B-11D1-8B70-080036B11A03} 13", "size"];
-							for (var i = col.length; i--;) {
-								var s = api.PSFormatForDisplay(col[i], Item.ExtendedProperty(col[i]), PDFF_DEFAULT);
-								ar[i + 1] = " " + api.PSGetDisplayName(col[i]) + ": " + s;
-							}
-						}
-						ar.push(new Array(Math.floor(x / Addons.TooltipPreview.SIZE.cx * 1.5)).join(" "));
-						return ar.join(" \n");
 					}
+					var ar = new Array(Math.floor(y / Addons.TooltipPreview.SIZE.cy));
+					var col = ["name", "type", "write", "{6444048F-4C8B-11D1-8B70-080036B11A03} 13", "size"];
+					for (var i = col.length; i--;) {
+						var s = api.PSFormatForDisplay(col[i], Item.ExtendedProperty(col[i]), PDFF_DEFAULT);
+						ar[i + 1] = " " + api.PSGetDisplayName(col[i]) + ": " + s;
+					}
+					ar.push(new Array(Math.floor(x / Addons.TooltipPreview.SIZE.cx * 1.5)).join(" "));
+					return ar.join(" \n");
 				}
 			}
 		}
