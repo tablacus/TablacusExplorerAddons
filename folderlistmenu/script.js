15a70b42740b1548e2c8d1ed72fa73b8e50ff094
Gaku <rxf10242@gmail.com>
2017-06-07 21:52:54 +0900

    17.6.7

--- folderlistmenu/script.js
+++ folderlistmenu/script.js
@@ -7,7 +7,8 @@
 			if (FV) {
 				FV.Focus();
 			}
-			var hMenu = api.CreatePopupMenu();
+			var oMenu = {'\\' : api.CreatePopupMenu()};
+			var strMenu = [];
 			var items = [];
 			s = api.PathUnquoteSpaces(s);
 			var ado = OpenAdodbFromTextFile(s);
@@ -18,7 +19,7 @@
 			mii.fMask = MIIM_ID | MIIM_BITMAP | MIIM_SUBMENU | MIIM_DATA | MIIM_STRING;
 			MenusIcon(mii, "icon:shell32.dll,3,16");
 			while (!ado.EOS) {
-				var path = api.PathUnquoteSpaces(ado.ReadText(adReadLine));
+				var path = api.PathUnquoteSpaces(ExtractMacro(te, ado.ReadText(adReadLine)));
 				if (/^#/.test(path)) {
 					res = /#EXTINF:[\d\s\-]+,\s*(.*)/i.exec(path);
 					if (res) {
@@ -26,17 +27,43 @@
 					}
 					continue;
 				}
-				res = /^(.*?)[\s\-]*\t(.*)$/.exec(path) || /^(.*?)[\s\-]*([a-z]:\\.*)$/i.exec(path) || /^(.*?)[\s\-]*(\\\\.*)$/.exec(path) || /^(.*?)[\s\-]*(::{.*)$/.exec(path) || /^(.*?)[\s\-]*([a-z]+:\/\/.*)$/.exec(path);
+				res = /^(.*?)[\s\-]*\t(.*)$/.exec(path);
+				if (res) {
+					if (res[1]) {
+						Name = res[1];
+						path = res[2];
+					}
+				}
+				res = /^(.*?)[\s\-]*([a-z]:\\.*)$/i.exec(path) || /^(.*?)[\s\-]*(\\\\.*)$/.exec(path) || /^(.*?)[\s\-]*(::{.*)$/.exec(path) || /^(.*?)[\s\-]*([a-z]+:\/\/.*)$/.exec(path);
 				if (res) {
 					if (res[1]) {
 						Name = res[1];
 						path = res[2];
 					}
 				} else {
-					path = fso.BuildPath(Parent, path);
+					path = fso.BuildPath(Parent, path.replace(/\//g, "\\"));
 				}
 				items.push(path);
-				mii.dwTypeData = Name || fso.GetFileName(path);
+				var arName = (Name || fso.GetFileName(path) || path.replace(/\\/g, "")).split(/\\|\/|\|/);
+				var strPath = '';
+				var hMenu = oMenu['\\'];
+				while (arName.length > 1) {
+					var s = arName.shift();
+					if (s) {
+						strPath += '\\' + s;
+						if (oMenu[strPath]) {
+							hMenu = oMenu[strPath];
+						} else {
+							mii.dwTypeData = s;
+							mii.hSubMenu = api.CreateMenu();
+							oMenu[strPath] = mii.hSubMenu;
+							api.InsertMenuItem(hMenu, MAXINT, false, mii);
+							hMenu = mii.hSubMenu;
+						}
+					}
+				}
+				mii.dwTypeData = arName.pop();
+				mii.hSubMenu = 0;
 				mii.wID = items.length;
 				api.InsertMenuItem(hMenu, MAXINT, false, mii);
 				Name = "";
@@ -49,8 +76,10 @@
 				}
 			}
 			window.g_menu_click = true;
-			var nVerb = api.TrackPopupMenuEx(hMenu, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_RIGHTBUTTON | TPM_RETURNCMD, pt.x, pt.y, te.hwnd, null);
-			api.DestroyMenu(hMenu);
+			var nVerb = api.TrackPopupMenuEx(oMenu['\\'], TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_RIGHTBUTTON | TPM_RETURNCMD, pt.x, pt.y, te.hwnd, null);
+			for (var i in oMenu) {
+				api.DestroyMenu(oMenu[i]);
+			}
 			if (nVerb) {
 				var path = items[nVerb - 1];
 				switch (window.g_menu_button - 0) {
