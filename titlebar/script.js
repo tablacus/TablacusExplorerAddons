1261776d4d40afa06b56fb7e27de81006d7687a2
Gaku <rxf10242@gmail.com>
2017-09-10 23:47:19 +0900

    17.9.10

--- titlebar/script.js
+++ titlebar/script.js
@@ -2,9 +2,12 @@
 	AddEvent("ChangeView", function(Ctrl)
 	{
 		try {
-			if (Ctrl.Id == Ctrl.Parent.Selected.Id && Ctrl.Parent.Id == te.Ctrl(CTRL_TC).Id) {
-				api.SetWindowText(te.hwnd, Ctrl.Title + ' - Tablacus Explorer');
-			}
+			setTimeout(function ()
+			{
+				if (Ctrl.Id == Ctrl.Parent.Selected.Id && Ctrl.Parent.Id == te.Ctrl(CTRL_TC).Id) {
+					api.SetWindowText(te.hwnd, Ctrl.Title + ' - Tablacus Explorer');
+				}
+			}, 99);
 		} catch (e) {}
 	});
 }
