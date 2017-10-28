0e54ff9de818351739b8054fa7cba6fec26fb4b9
Gaku <rxf10242@gmail.com>
2017-10-29 00:49:24 +0900

    17.10.28 Added: Fix Windows 10 Fall Creators Update bug

---
+++ fixwin10fcu/script.js
@@ -0,0 +1,17 @@
+﻿if (window.Addon == 1) {
+	Addons.FixWin10FCU = {};
+
+	AddEvent("MouseMessage", function (Ctrl, hwnd, msg, wParam, pt)
+	{
+		if (msg == WM_MOUSEMOVE) {
+			if (api.GetKeyState(VK_LBUTTON) < 0) {
+				if (pt.x == 0 || pt.y == 0 || Math.abs(pt.x - screen.width * screen.deviceXDPI / screen.logicalXDPI) < 2 || Math.abs(pt.y - screen.height * screen.deviceYDPI / screen.logicalYDPI) < 2) {
+					api.SetCursorPos(Addons.FixWin10FCU.x, Addons.FixWin10FCU.y);
+				} else {
+					Addons.FixWin10FCU.x = pt.x;
+					Addons.FixWin10FCU.y = pt.y;
+				}
+			}
+		}
+	});
+}
