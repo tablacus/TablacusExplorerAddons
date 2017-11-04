683168daeafc3d5a61a556188e13dd364da561c7
Gaku <rxf10242@gmail.com>
2017-11-04 23:03:44 +0900

    17.11.4

--- fixwin10fcu/script.js
+++ fixwin10fcu/script.js
@@ -1,16 +1,9 @@
 ﻿if (window.Addon == 1) {
-	Addons.FixWin10FCU = {};
-
 	AddEvent("MouseMessage", function (Ctrl, hwnd, msg, wParam, pt)
 	{
-		if (msg == WM_MOUSEMOVE) {
-			if (api.GetKeyState(VK_LBUTTON) < 0) {
-				if (pt.x == 0 || pt.y == 0 || Math.abs(pt.x - screen.width * screen.deviceXDPI / screen.logicalXDPI) < 2 || Math.abs(pt.y - screen.height * screen.deviceYDPI / screen.logicalYDPI) < 2) {
-					api.SetCursorPos(Addons.FixWin10FCU.x, Addons.FixWin10FCU.y);
-				} else {
-					Addons.FixWin10FCU.x = pt.x;
-					Addons.FixWin10FCU.y = pt.y;
-				}
+		if (Ctrl.Type == CTRL_SB && msg == WM_MOUSEMOVE) {
+			if (api.GetKeyState(VK_LBUTTON) < 0 || api.GetKeyState(VK_RBUTTON) < 0) {
+				api.RedrawWindow(Ctrl.hwnd, null, 0, RDW_NOERASE | RDW_INVALIDATE);
 			}
 		}
 	});
