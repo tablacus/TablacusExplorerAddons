7264f13a7e31b05af9f71d2a6ca9f674f1e9327f
Gaku <rxf10242@gmail.com>
2018-03-17 22:01:29 +0900

    18.3.17

--- mainmenubutton/script.js
+++ mainmenubutton/script.js
@@ -1,5 +1,5 @@
-﻿Addon_Id = "mainmenubutton";
-Default = "ToolBar1Left";
+﻿var Addon_Id = "mainmenubutton";
+var Default = "ToolBar1Left";
 
 var items = te.Data.Addons.getElementsByTagName(Addon_Id);
 if (items.length) {
@@ -27,84 +27,82 @@ if (window.Addon == 1) {
 				clearTimeout(Addons.MainMenuButton.tid2);
 				Addons.MainMenuButton.tid2 = setTimeout("Addons.MainMenuButton.bClose = false;", 500);
 			});
-			setTimeout(function () {
-				g_nPos = 0;
-				var hMenu = api.CreatePopupMenu();
-				for (var i = Addons.MainMenuButton.strMenus.length; i--;) {
-					var mii = api.Memory("MENUITEMINFO");
-					mii.cbSize = mii.Size;
-					mii.fMask = MIIM_STRING | MIIM_SUBMENU;
-					mii.dwTypeData = GetText(Addons.MainMenuButton.strMenus[i]);
-					mii.hSubMenu = api.CreatePopupMenu();
-					api.InsertMenu(mii.hSubMenu, 0, MF_BYPOSITION | MF_STRING, 0, api.sprintf(99, '\tJScript\tAddons.MainMenuButton.OpenSubMenu("%llx",%d,"%llx")', hMenu, i, mii.hSubMenu));
-					api.InsertMenuItem(hMenu, 0, true, mii);
-				}
-				Ctrl = Ctrl || te;
-				var ar = GetSelectedArray(Ctrl, pt);
-				var Selected = ar.shift();
-				var SelItem = ar.shift();
-				var FV = ar.shift();
-				ExtraMenuCommand = [];
-				ExtraMenuData = [];
-				eventTE.MenuCommand = [];
+			g_nPos = 0;
+			var hMenu = api.CreatePopupMenu();
+			for (var i = Addons.MainMenuButton.strMenus.length; i--;) {
+				var mii = api.Memory("MENUITEMINFO");
+				mii.cbSize = mii.Size;
+				mii.fMask = MIIM_STRING | MIIM_SUBMENU;
+				mii.dwTypeData = GetText(Addons.MainMenuButton.strMenus[i]);
+				mii.hSubMenu = api.CreatePopupMenu();
+				api.InsertMenu(mii.hSubMenu, 0, MF_BYPOSITION | MF_STRING, 0, api.sprintf(99, '\tJScript\tAddons.MainMenuButton.OpenSubMenu("%llx",%d,"%llx")', hMenu, i, mii.hSubMenu));
+				api.InsertMenuItem(hMenu, 0, true, mii);
+			}
+			Ctrl = Ctrl || te;
+			var ar = GetSelectedArray(Ctrl, pt);
+			var Selected = ar.shift();
+			var SelItem = ar.shift();
+			var FV = ar.shift();
+			ExtraMenuCommand = [];
+			ExtraMenuData = [];
+			eventTE.menucommand = [];
 
-				if (!pt && o) {
-					pt = GetPos(o, true);
-					pt.y = pt.y + o.offsetHeight;
-				}
-				if (!pt) {
-					pt = api.Memory("POINT");
-					var hwnd = api.FindWindowEx(FV.hwndView, 0, WC_LISTVIEW, null);
-					if (hwnd) {
-						var rc = api.Memory("RECT");
-						var i = FV.GetFocusedItem;
-						if (api.SendMessage(hwnd, LVM_ISITEMVISIBLE, i, 0)) {
-							rc.Left = LVIR_LABEL;
-							api.SendMessage(hwnd, LVM_GETITEMRECT, i, rc);
-							pt.x = rc.Left;
-							pt.y = (rc.Top + rc.Bottom) / 2;
-						}
-					}
-					api.ClientToScreen(FV.hwnd, pt);
-				}
-				Addons.MainMenuButton.Ctrl = Ctrl;
-				Addons.MainMenuButton.pt = pt;
-				Addons.MainMenuButton.FV = FV;
-				Addons.MainMenuButton.ContextMenu = [];
-				Addons.MainMenuButton.Selected = Selected;
-				Addons.MainMenuButton.SelItem = SelItem;
-				Addons.MainMenuButton.uCMF = Ctrl.Type != CTRL_TV ? CMF_NORMAL | CMF_CANRENAME : CMF_EXPLORE | CMF_CANRENAME;
-				Addons.MainMenuButton.arItem = {};
-				if (api.GetKeyState(VK_SHIFT) < 0) {
-					Addons.MainMenuButton.uCMF |= CMF_EXTENDEDVERBS;
-				}
-				var nVerb = api.TrackPopupMenuEx(hMenu, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_RIGHTBUTTON | TPM_RETURNCMD, pt.x, pt.y, te.hwnd, null, Addons.MainMenuButton.ContextMenu);
-				var Name = Addons.MainMenuButton.strMenus[0].replace("&", "");
-				for (var i = Addons.MainMenuButton.strMenus.length; i--;) {
-					api.GetMenuItemInfo(hMenu, i, true, mii);
-					mii.fMask = MIIM_SUBMENU;
-					if (api.GetMenuString(mii.hSubMenu, nVerb, MF_BYCOMMAND)) {
-						Name = Addons.MainMenuButton.strMenus[i].replace("&", "");
+			if (!pt && o) {
+				pt = GetPos(o, true);
+				pt.y = pt.y + o.offsetHeight * screen.deviceYDPI / screen.logicalYDPI;;
+			}
+			if (!pt) {
+				pt = api.Memory("POINT");
+				var hwnd = api.FindWindowEx(FV.hwndView, 0, WC_LISTVIEW, null);
+				if (hwnd) {
+					var rc = api.Memory("RECT");
+					var i = FV.GetFocusedItem;
+					if (api.SendMessage(hwnd, LVM_ISITEMVISIBLE, i, 0)) {
+						rc.Left = LVIR_LABEL;
+						api.SendMessage(hwnd, LVM_GETITEMRECT, i, rc);
+						pt.x = rc.Left;
+						pt.y = (rc.Top + rc.Bottom) / 2;
 					}
 				}
-				if (nVerb) {
-					var mii = api.Memory("MENUITEMINFO");
-					mii.cbSize = mii.Size;
-					mii.fMask = MIIM_SUBMENU;
-					var hr = ExecMenu4(Ctrl, Name, pt, hMenu, Addons.MainMenuButton.ContextMenu, nVerb, Addons.MainMenuButton.FV);
-					if (isFinite(hr)) {
-						return hr;
-					}
-					var item = Addons.MainMenuButton.arItem[nVerb - 1];
-					if (item) {
-						var s = item.getAttribute("Type");
-						Exec(Ctrl, item.text, window.g_menu_button == 3 && s == "Open" ? "Open in New Tab" : s, Ctrl.hwnd, pt);
-						return S_OK;
-					}
-				} else {
-					api.DestroyMenu(hMenu);
+				api.ClientToScreen(FV.hwnd, pt);
+			}
+			Addons.MainMenuButton.Ctrl = Ctrl;
+			Addons.MainMenuButton.pt = pt;
+			Addons.MainMenuButton.FV = FV;
+			Addons.MainMenuButton.ContextMenu = [];
+			Addons.MainMenuButton.Selected = Selected;
+			Addons.MainMenuButton.SelItem = SelItem;
+			Addons.MainMenuButton.uCMF = Ctrl.Type != CTRL_TV ? CMF_NORMAL | CMF_CANRENAME : CMF_EXPLORE | CMF_CANRENAME;
+			Addons.MainMenuButton.arItem = {};
+			if (api.GetKeyState(VK_SHIFT) < 0) {
+				Addons.MainMenuButton.uCMF |= CMF_EXTENDEDVERBS;
+			}
+			var nVerb = api.TrackPopupMenuEx(hMenu, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_RIGHTBUTTON | TPM_RETURNCMD, pt.x, pt.y, te.hwnd, null, Addons.MainMenuButton.ContextMenu);
+			var Name = Addons.MainMenuButton.strMenus[0].replace("&", "");
+			for (var i = Addons.MainMenuButton.strMenus.length; i--;) {
+				api.GetMenuItemInfo(hMenu, i, true, mii);
+				mii.fMask = MIIM_SUBMENU;
+				if (api.GetMenuString(mii.hSubMenu, nVerb, MF_BYCOMMAND)) {
+					Name = Addons.MainMenuButton.strMenus[i].replace("&", "");
 				}
-			}, 99);
+			}
+			if (nVerb) {
+				var mii = api.Memory("MENUITEMINFO");
+				mii.cbSize = mii.Size;
+				mii.fMask = MIIM_SUBMENU;
+				var hr = ExecMenu4(Ctrl, Name, pt, hMenu, Addons.MainMenuButton.ContextMenu, nVerb, Addons.MainMenuButton.FV);
+				if (isFinite(hr)) {
+					return hr;
+				}
+				var item = Addons.MainMenuButton.arItem[nVerb - 1];
+				if (item) {
+					var s = item.getAttribute("Type");
+					Exec(Ctrl, item.text, window.g_menu_button == 3 && s == "Open" ? "Open in new tab" : s, Ctrl.hwnd, pt);
+					return S_OK;
+				}
+			} else {
+				api.DestroyMenu(hMenu);
+			}
 		},
 
 		Exec: function (Ctrl, pt)
@@ -124,9 +122,6 @@ if (window.Addon == 1) {
 				var nBase = api.QuadPart(menus[0].getAttribute("Base"));
 				Addons.MainMenuButton.ContextMenu.push(GetBaseMenuEx(hMenu, nBase, Addons.MainMenuButton.FV, Addons.MainMenuButton.Selected, Addons.MainMenuButton.uCMF, 0, Addons.MainMenuButton.SelItem, Addons.MainMenuButton.ContextMenu));
 				if (nBase < 5) {
-					var mii = api.Memory("MENUITEMINFO");
-					mii.cbSize = mii.Size;
-					mii.fMask = MIIM_FTYPE;
 					AdjustMenuBreak(hMenu);
 				}
 				var arMenu;
@@ -135,8 +130,21 @@ if (window.Addon == 1) {
 					var x = g_nPos;
 					arMenu = OpenMenu(items, Addons.MainMenuButton.SelItem);
 					g_nPos = MakeMenus(hMenu, menus, arMenu, items, Ctrl, pt, g_nPos, Addons.MainMenuButton.arItem);
-					for (var i in eventTE[Name]) {
-						g_nPos = eventTE[Name][i](Addons.MainMenuButton.Ctrl, hMenu, g_nPos, Addons.MainMenuButton.Selected, Addons.MainMenuButton.SelItem, Addons.MainMenuButton.ContextMenu[0]);
+
+					var eo = eventTE[Name.toLowerCase()];
+					for (var i in eo) {
+						try {
+							g_nPos = eo[i](Addons.MainMenuButton.Ctrl, hMenu, g_nPos, Addons.MainMenuButton.Selected, Addons.MainMenuButton.SelItem, Addons.MainMenuButton.ContextMenu[0], Name, Addons.MainMenuButton.pt);
+						} catch (e) {
+							ShowError(e, Name, i);
+						}
+					}
+					for (var i in eventTE.menus) {
+						try {
+							g_nPos = eventTE.menus[i](Addons.MainMenuButton.Ctrl, hMenu, g_nPos, Addons.MainMenuButton.Selected, Addons.MainMenuButton.SelItem, Addons.MainMenuButton.ContextMenu[0], Name, Addons.MainMenuButton.pt);
+						} catch (e) {
+							ShowError(e, "Menus", i);
+						}
 					}
 				}
 			}
@@ -159,7 +167,7 @@ if (window.Addon == 1) {
 
 	}
 	AddTypeEx("Add-ons", "Main Menu", Addons.MainMenuButton.Exec);
-	
+
 	AddEvent("KeyMessage", function (Ctrl, hwnd, msg, key, keydata)
 	{
 		if (msg == WM_KEYDOWN || msg == WM_SYSKEYDOWN) {
