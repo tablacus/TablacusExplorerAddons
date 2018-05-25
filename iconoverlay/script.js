b8d0642df29e7fa650012b3182046a86094e3c73
Gaku <rxf10242@gmail.com>
2018-05-26 00:31:01 +0900

    18.5.25

--- iconoverlay/script.js
+++ iconoverlay/script.js
@@ -5,6 +5,7 @@ if (window.Addon == 1) {
 	{
 		DLL: api.DllGetClassObject(fso.BuildPath(fso.GetParentFolderName(api.GetModuleFileName(null)), ["addons\\iconoverlay\\ticonoverlay", api.sizeof("HANDLE") * 8, ".dll"].join("")), "{ADB2CB70-5C00-4fa2-B121-CB60B556FFA7}"),
 		Icon: [],
+		FV: {},
 
 		Finalize: function ()
 		{
@@ -19,38 +20,46 @@ if (window.Addon == 1) {
 		}
 	});
 
-	AddEvent("ItemPostPaint2", function (Ctrl, pid, nmcd, vcd)
-	{
-		var hList = Ctrl.hwndList;
-		if (hList && pid && Addons.IconOverlay.DLL) {
-			var i = Addons.IconOverlay.DLL.GetOverlayIconIndex(pid.Path, 0);
-			if (i >= 0) {
-				var image = Addons.IconOverlay.Icon[i];
-				if (image) {
-					var rc = api.Memory("RECT");
-					rc.Left = LVIR_ICON;
-					api.SendMessage(hList, LVM_GETITEMRECT, nmcd.dwItemSpec, rc);
-					var x, y, w = Ctrl.IconSize * screen.logicalYDPI / 96;
-					image = GetThumbnail(image, w, Ctrl.IconSize >= 32);
-					if (api.SendMessage(hList, LVM_GETVIEW, 0, 0)) {
-						x = rc.Right - w + 1;
-						y = rc.Bottom - w - 3;
-					} else {
-						x = rc.Left + (rc.Right - rc.Left - w) / 2;
-						y = rc.Bottom - image.GetHeight() - 1;
-					}
-					if (image) {
-						image.DrawEx(nmcd.hdc, x, y, 0, 0, CLR_NONE, CLR_NONE, ILD_NORMAL);
-					}
-				}
-			}
-		}
-	});
-	
 	if (Addons.IconOverlay.DLL) {
 		Addons.IconOverlay.DLL.init(GetAddonOption(Addon_Id, "Base") || 11);
 		for (var i = 0, o = {}; Addons.IconOverlay.DLL.GetOverlayInfo(i, o) == S_OK; i++) {
-			Addons.IconOverlay.Icon[i] = o.dwFlags == 1 ? api.CreateObject("WICBitmap").FromFile(o.IconFile) : MakeImgData("icon:" + o.IconFile + "," + o.Index, 0, 32);
+			Addons.IconOverlay.Icon[i] = [MakeImgData("icon:" + o.IconFile + (o.dwFlags == 1 ? "" : "," + o.Index), 0, 16), o.dwFlags == 1 ? api.CreateObject("WICBitmap").FromFile(o.IconFile) : MakeImgData("icon:" + o.IconFile + "," + o.Index, 0, 32)];
+		}
+		if (Addons.IconOverlay.Icon.length) {		
+			AddEvent("ListViewCreated", function (Ctrl)
+			{
+				Addons.IconOverlay.FV[Ctrl.Id] = {};
+			});
+			AddEvent("ItemPostPaint2", function (Ctrl, pid, nmcd, vcd)
+			{
+				var hList = Ctrl.hwndList;
+				if (hList && pid && Addons.IconOverlay.DLL) {
+					var db = Addons.IconOverlay.FV[Ctrl.Id];
+					if (!db) {
+						db = {};
+						Addons.IconOverlay.FV[Ctrl.Id] = db;
+					}
+					var i = db[pid.Path];
+					if (isNaN(i)) {
+						i = Addons.IconOverlay.DLL.GetOverlayIconIndex(pid.Path, api.GetAttributesOf(pid, SFGAO_STORAGECAPMASK));
+						db[pid.Path] = i;
+					}
+					var icon = Addons.IconOverlay.Icon[i];
+					if (icon) {
+						var image = icon[Ctrl.IconSize < 32 ? 0 : 1];
+						var rc = api.Memory("RECT");
+						rc.Left = LVIR_ICON;
+						api.SendMessage(hList, LVM_GETITEMRECT, nmcd.dwItemSpec, rc);
+						var w = Ctrl.IconSize * screen.logicalYDPI / 96;
+						image = GetThumbnail(image, Ctrl.IconSize < 32 ? w : (w * 5 / 16) + 16);
+						var x = api.SendMessage(hList, LVM_GETVIEW, 0, 0) == 2 ? rc.Right - w : rc.Left + (rc.Right - rc.Left - w) / 2;
+						var y = rc.Bottom - image.GetHeight() - 2;
+						if (image) {
+							image.DrawEx(nmcd.hdc, x, y, 0, 0, CLR_NONE, CLR_NONE, ILD_NORMAL);
+						}
+					}
+				}
+			});
 		}
 	}
 } else {
