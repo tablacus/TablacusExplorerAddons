a2ac91e94c7d256a5b0af27baa8c31a5fec5a5b5
Gaku <rxf10242@gmail.com>
2018-07-26 23:35:52 +0900

    18.7.26 Added: Open the same locked tab instead

--- preventduplicatetabs/script.js
+++ preventduplicatetabs/script.js
@@ -1,29 +1,26 @@
 ﻿if (window.Addon == 1) {
 	AddEvent("BeforeNavigate", function (Ctrl, fs, wFlags, Prev)
 	{
+		var hr;
 		var db = {};
 		var TC = Ctrl.Parent;
 		for (var i = TC.Count; i-- > 0;) {
 			var Item = TC.Item(i);
 			if (Ctrl.hwnd != Item.hwnd && api.ILIsEqual(Ctrl.FolderItem, Item.FolderItem)) {
-				if (!(wFlags & SBSP_ACTIVATE_NOFOCUS) || TC.Selected.hwnd == Ctrl.hwnd) {
-					(function (TC, cFV) { setTimeout(function () {
-						var Selected = cFV[0].SelectedItems();
-						for (var i = 2; i--;) {
-							if (cFV[i].Close()) {
-								TC.SelectedIndex = cFV[i ^ 1].Index;
-								if (i == 0 && Selected.Count == 1) {
-									setTimeout(function () {
-										cFV[1].SelectItem(Selected.Item(0), SVSI_FOCUSED | SVSI_ENSUREVISIBLE | SVSI_NOTAKEFOCUS | SVSI_DESELECTOTHERS | SVSI_SELECT);
-									}, 99);
-								}
-								break;
-							}
+				if (Item.Data.Lock) {
+					if (!(wFlags & SBSP_ACTIVATE_NOFOCUS) || TC.Selected.hwnd == Ctrl.hwnd) {
+						var Selected = Ctrl.SelectedItems();
+						TC.SelectedIndex = i;
+						if (Selected.Count == 1) {
+							setTimeout(function () {
+								Item.SelectItem(Selected.Item(0), SVSI_FOCUSED | SVSI_ENSUREVISIBLE | SVSI_NOTAKEFOCUS | SVSI_DESELECTOTHERS | SVSI_SELECT);
+							}, 99);
 						}
-					}, 99);}) (TC, [Ctrl, Item]);
-					continue;
+					}
+					hr = E_ABORT;
+				} else if (!hr) {
+					Item.Close();
 				}
-				return E_FAIL;
 			}
 			var path = api.GetDisplayNameOf(Item, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_FORPARSINGEX);
 			if (db[path]) {
@@ -51,7 +48,8 @@
 				}
 			}
 		}, 99);
-	});
+		return hr;
+	}, true);
 
 	if (!HOME_PATH) {
 		HOME_PATH = ssfRECENT;
---
+++ samelockedtab/script.js
@@ -0,0 +1,23 @@
+﻿if (window.Addon == 1) {
+	AddEvent("BeforeNavigate", function (Ctrl, fs, wFlags, Prev)
+	{
+		var hr;
+		var TC = Ctrl.Parent;
+		for (var i = TC.Count; i-- > 0;) {
+			var Item = TC.Item(i);
+			if (Item.Data.Lock && Ctrl.hwnd != Item.hwnd && api.ILIsEqual(Ctrl.FolderItem, Item.FolderItem)) {
+				if (!(wFlags & SBSP_ACTIVATE_NOFOCUS) || TC.Selected.hwnd == Ctrl.hwnd) {
+					var Selected = Ctrl.SelectedItems();
+					TC.SelectedIndex = i;
+					if (Selected.Count == 1) {
+						setTimeout(function () {
+							Item.SelectItem(Selected.Item(0), SVSI_FOCUSED | SVSI_ENSUREVISIBLE | SVSI_NOTAKEFOCUS | SVSI_DESELECTOTHERS | SVSI_SELECT);
+						}, 99);
+					}
+				}
+				hr = E_ABORT;
+			}
+		}
+		return hr;
+	});
+}
