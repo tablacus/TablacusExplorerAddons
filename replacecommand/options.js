2ceb4fa1bf6aea71b2b2e36b9776fa65f52a10dc
Gaku <rxf10242@gmail.com>
2018-11-15 22:30:30 +0900

    18.11.15

---
+++ replacecommand/options.js
@@ -0,0 +1,110 @@
+var ado = OpenAdodbFromTextFile("addons\\" + Addon_Id + "\\options.html");
+if (ado) {
+	SetTabContents(0, "General", ado.ReadText(adReadAll));
+	ado.Close();
+}
+
+Addons.ReplaceCommand =
+{
+	BrowseFile1: function ()
+	{
+		var FV = GetFolderView();
+		var path = OpenDialog(fso.GetParentFolderName(document.F.elements["_file"].value) || FV.FolderItem.Path);
+		if (path) {
+			var s = document.F.re.value;
+			if (s.indexOf("$1") < 0) {
+				AddPath("re", path);
+			} else {
+				document.F.re.value = s.replace("$1", path);
+			}
+		}
+	},
+
+	BrowseFile: function ()
+	{
+		var FV = GetFolderView();
+		var path = OpenDialog(fso.GetParentFolderName(document.F.elements["_file"].value) || FV.FolderItem.Path);
+		if (path) {
+			document.F.elements["_file"].value = path;
+		}
+	},
+
+	BrowseVerb: function (o)
+	{
+		var hMenu = api.CreatePopupMenu();
+		var Item = this.GetItem();
+		var ContextMenu = api.ContextMenu(Item);
+		if (ContextMenu) {
+			ContextMenu.QueryContextMenu(hMenu, 0, 1, 0x7FFF, CMF_EXTENDEDVERBS | CMF_DONOTPICKDEFAULT | CMF_ITEMMENU);
+			var mii = api.Memory("MENUITEMINFO");
+			mii.cbSize = mii.Size;
+			mii.fMask = MIIM_ID;
+			for (var i = api.GetMenuItemCount(hMenu); i--;) {
+				api.GetMenuItemInfo(hMenu, i, true, mii);
+				var strVerb = ContextMenu.GetCommandString(mii.wID - ContextMenu.idCmdFirst, GCS_VERB);
+				if (!strVerb || !api.AssocQueryString(ASSOCF_NONE, ASSOCSTR_COMMAND, Item, (strVerb == "default" || !strVerb) ? null : strVerb)) {
+					api.DeleteMenu(hMenu, i, MF_BYPOSITION);
+				}
+			}
+			pt = GetPos(o, true);
+			pt.y += o.offsetHeight;
+			var nVerb = api.TrackPopupMenuEx(hMenu, TPM_RIGHTBUTTON | TPM_RETURNCMD, pt.x, pt.y, te.hwnd, null, ContextMenu);
+			document.F.elements["_verb"].value = ContextMenu.GetCommandString(nVerb - ContextMenu.idCmdFirst, GCS_VERB) || "default";
+		}
+		api.DestroyMenu(hMenu);
+		this.Get();
+	},
+
+	Get: function ()
+	{
+		var strVerb = document.F.elements["_verb"].value;
+		document.F.elements["_aqs"].value = api.AssocQueryString(ASSOCF_NONE, ASSOCSTR_COMMAND, this.GetItem(), (strVerb == "default" || !strVerb) ? null : strVerb);
+	},
+
+	SetRE: function ()
+	{
+		var s = document.F.elements["_aqs"].value;
+		if (!s || /^[\/\|!#]/.test(s)) {
+			return;
+		}
+		var spliter;
+		var ar = ["/", "|", "!",  "#"];
+		for (var i = 0; i < ar.length; i++) {
+			spliter = ar[i];
+			if (s.indexOf(spliter) < 0) {
+				break;
+			}
+		}
+		var sr = s.replace(/([\+\*\.\?\^\$\[\-\]\|\(\)\\])/g, "\\$1");
+		var arg = api.CommandLineToArgv(sr);
+		var d = /^"/.test(s) ? 2 : 0;
+		sr = '^(' + sr.substr(0, arg[0].length + d) + ')' + sr.substr(arg[0].length + d) + "$";
+		var arg = api.CommandLineToArgv(s);
+		s = "$1" + s.substr(arg[0].length + d);
+		document.F.elements["_aqs"].value = spliter + sr + spliter + s + spliter + "i";
+	},
+
+	Add: function ()
+	{
+		var s = document.F.elements["_aqs"].value;
+		if (/^[\/\|!#]/.test(s)) {
+			var r = document.F.re.value;
+			if (!r || /\n$/.test(r)) {
+				document.F.re.value += s;
+			} else {
+				document.F.re.value += "\n" + s;
+			}
+		}
+	},
+
+	GetItem: function ()
+	{
+		var Item = document.F.elements["_file"].value;
+		if (!Item) {
+			var FV = GetFolderView();
+			var Selected = FV.SelectedItems();
+			Item = Selected.Count ? Selected.Item(0) : FV.FolderItem;
+		}
+		return Item;
+	}
+}
--- replacecommand/script.js
+++ replacecommand/script.js
@@ -1,26 +1,28 @@
 var Addon_Id = "replacecommand";
-(function () {
-	var items = te.Data.Addons.getElementsByTagName(Addon_Id);
-	if (items.length) {
-		var item = items[0];
-		if (!item.getAttribute("Set")) {
-			item.setAttribute("re", "/^(notepad\\.exe)/$1/i");
-		}
-	}
-	if (window.Addon == 1) {
-		AddEvent("ExtractMacro", [/./, function(Ctrl, s, re)
-		{
-			var lines = GetAddonOption("replacecommand", "re").split("\n");
-			for (var i in lines) {
-				var line = lines[i];
-				if (line.length) {
-					var re = line.split(line.charAt(0));
-					if (re.length > 3) {
-						s = s.replace(new RegExp(re[1], re[3]), re[2]);
-					}
+
+var item = GetAddonElement(Addon_Id);
+if (!item.getAttribute("Set")) {
+	item.setAttribute("re", "/^(notepad\\.exe)/$1/i");
+}
+Addons.ReplaceCommand = {
+	lines: GetAddonOption("replacecommand", "re").split("\n")
+}
+
+if (window.Addon == 1) {
+	AddEvent("ExtractMacro", [/./, function(Ctrl, s, re)
+	{
+		var lines = Addons.ReplaceCommand.lines;
+		for (var i = 0; i < lines.length; i++) {
+			var line = lines[i];
+			if (line.length) {
+				var re = line.split(line.charAt(0));
+				if (re.length > 3) {
+					s = s.replace(new RegExp(re[1], re[3]), re[2]);
 				}
 			}
-			return s;
-		}]);
-	}
-})();
+		}
+		return s;
+	}]);
+} else {
+	importScript("addons\\" + Addon_Id + "\\options.js");
+}
