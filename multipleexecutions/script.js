1fac7fc9f7156a60e3096b7b74714abf6cb84aa4
Gaku <rxf10242@gmail.com>
2018-12-06 21:41:46 +0900

    18.12.6 Added: Multiple executions

---
+++ multipleexecutions/script.js
@@ -0,0 +1,9 @@
+﻿if (window.Addon == 1) {
+	AddEvent("CopyData", function (Ctrl, cd, wParam)
+	{
+		if (Ctrl.Type == CTRL_TE && cd.dwData == 0 && cd.cbData) {
+			Finalize();
+			return S_FALSE;
+		}
+	});
+}
