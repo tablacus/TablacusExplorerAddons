990599ef21c3fe4e6fe8a2b4bc1905041daf24b6
Gaku <rxf10242@gmail.com>
2018-12-15 21:42:18 +0900

    18.12.15

--- fixselection/script.js
+++ fixselection/script.js
@@ -1,4 +1,37 @@
 ﻿if (window.Addon == 1) {
+	Addons.FixSelection = {
+		tid: {},
+
+		Exec: function (FV)
+		{
+			if (FV.Type == CTRL_SB && FV.Data) {
+				var Selected = FV.Data.Selected;
+				if (Selected && Selected.Count != FV.ItemCount(SVGIO_SELECTION)) {
+					FV.SelectItem(null, SVSI_DESELECTOTHERS);
+					for (var i = Selected.Count; i--;) {
+						FV.SelectItem(Selected.Item(i), SVSI_SELECT);
+					}
+				}
+			}
+		},
+
+		ChangeNotify: function (FV, pid)
+		{
+			if (FV.Type == CTRL_SB && FV.Data) {
+				if (FV.Data.Selected && api.ILIsEqual(FV.FolderItem, pid)) {
+					if (Addons.FixSelection.tid[FV.Id]) {
+						clearTimeout(Addons.FixSelection.tid[FV.Id]);
+					}
+					Addons.FixSelection.tid[FV.Id] = setTimeout(function ()
+					{
+						delete Addons.FixSelection.tid[FV.Id];
+						Addons.FixSelection.Exec(FV);
+					}, 99)
+				}
+			}
+		}
+	}
+
 	AddEvent("BeforeNavigate", function (Ctrl, fs, wFlags, Prev)
 	{
 		Ctrl.Data.Selected = null;
@@ -6,7 +39,7 @@
 
 	AddEvent("SelectionChanged", function (Ctrl, uChange)
 	{
-		if (Ctrl.Type <= CTRL_EB && Ctrl.Data) {
+		if (Ctrl.Type == CTRL_SB && Ctrl.Data) {
 			var Selected = Ctrl.SelectedItems();
 			if (Selected.Count == 0) {
 				setTimeout(function ()
@@ -21,13 +54,14 @@
 		}
 	});
 
-	AddEvent("Sort", function (Ctrl)
+	AddEvent("Sort", Addons.FixSelection.Exec);
+
+	AddEvent("ChangeNotify", function (Ctrl, pidls, wParam, lParam)
 	{
-		var Selected = Ctrl.Data.Selected;
-		if (Selected) {
-			Ctrl.SelectItem(null, SVSI_DESELECTOTHERS);
-			for (var i = Selected.Count; i--;) {
-				Ctrl.SelectItem(Selected.Item(i), SVSI_SELECT);
+		if (pidls.lEvent == SHCNE_UPDATEDIR) {
+			var cFV = te.Ctrls(CTRL_FV);
+			for (var i in cFV) {
+				Addons.FixSelection.ChangeNotify(cFV[i], pidls[0]);
 			}
 		}
 	});
