8774aa1d18c37b9f12c292b50b281cafc6a2055d
Gaku <rxf10242@gmail.com>
2018-12-26 22:19:22 +0900

    18.12.26
    Merged this.
    http://egg.5ch.net/test/read.cgi/software/1524903550/595
    
    [Image](http://hp.vector.co.jp/authors/VA040722/download/hotbuttonex.png)
    
    Download
    [hotbuttonex.zip](http://hp.vector.co.jp/authors/VA040722/download/hotbuttonex.zip)
    
    Thanks >>595.

--- hotbutton/script.js
+++ hotbutton/script.js
@@ -1,12 +1,12 @@
 var Addon_Id = "hotbutton";
 
 var item = GetAddonElement(Addon_Id);
-
 if (window.Addon == 1) {
 	Addons.HotButton =
 	{
 		rc: api.Memory("RECT"),
 		Select: item.getAttribute("Select"),
+		Handled: false,
 
 		IsHot: function (nmcd, hList)
 		{
@@ -18,14 +18,17 @@ if (window.Addon == 1) {
 		GetRect: function (hList, iItem, rc)
 		{
 			Addons.HotButton.GetRect2(hList, iItem, rc);
-			rc.Left = rc.Right - Addons.HotButton.Image.GetWidth();
-			rc.Top = (rc.Top + rc.Bottom - Addons.HotButton.Image.GetHeight()) / 2;
-			rc.Bottom = rc.Top + Addons.HotButton.Image.GetHeight();
+			rc.left = rc.right - Addons.HotButton.Image.GetWidth();
+			var h = Addons.HotButton.Image.GetHeight();
+			if (rc.bottom - rc.top > h * 2) {
+				rc.top = (rc.top + rc.bottom - h) / 2;
+				rc.bottom = rc.top + h;
+			}
 		},
 
 		GetRect2: function (hList, iItem, rc)
 		{
-			rc.Left = LVIR_SELECTBOUNDS;
+			rc.left = LVIR_SELECTBOUNDS;
 			api.SendMessage(hList, LVM_GETITEMRECT, iItem, rc);
 		}
 	};
@@ -38,11 +41,11 @@ if (window.Addon == 1) {
 				if (Addons.HotButton.Image) {
 					var rc = api.Memory("RECT");
 					Addons.HotButton.GetRect(hList, nmcd.dwItemSpec, rc);
-					if (rc.Left != Addons.HotButton.rc.Left || rc.Top != Addons.HotButton.rc.Top) {
+					if (rc.left != Addons.HotButton.rc.left || rc.top != Addons.HotButton.rc.top) {
 						api.InvalidateRect(hList, Addons.HotButton.rc, true);
 						Addons.HotButton.rc = rc;
 					}
-					Addons.HotButton.Image.DrawEx(nmcd.hdc, rc.Left, rc.Top, 0, 0, CLR_NONE, CLR_NONE, ILD_NORMAL);
+					Addons.HotButton.Image.DrawEx(nmcd.hdc, rc.left, (rc.top + rc.bottom - Addons.HotButton.Image.GetHeight()) / 2, 0, 0, CLR_NONE, CLR_NONE, ILD_NORMAL);
 				}
 			}
 		}
@@ -51,27 +54,44 @@ if (window.Addon == 1) {
 	AddEvent("MouseMessage", function (Ctrl, hwnd, msg, wParam, pt)
 	{
 		var hList = Ctrl.hwndList;
-		if (hList && msg == WM_LBUTTONDOWN) {
-			var iItem = Ctrl.HitTest(pt, LVHT_ONITEM);
-			if (iItem >= 0) {
-				var rc = api.Memory("RECT");
-				Addons.HotButton.GetRect(hList, iItem, rc);
-				var ptc = pt.Clone();
-				api.ScreenToClient(hList, ptc);
-				if (PtInRect(rc, ptc)) {
-					(function (Ctrl) { setTimeout(function ()
-					{
-						var Items = Ctrl.Items();
-						var Item = Items.Item(iItem);
+		if (hList) {
+			if (msg == WM_LBUTTONDOWN) {
+				Addons.HotButton.Handled = false;
+				var iItem = Ctrl.HitTest(pt, LVHT_ONITEM);
+				if (iItem >= 0) {
+					var rc = api.Memory("RECT");
+					Addons.HotButton.GetRect(hList, iItem, rc);
+					var ptc = pt.Clone();
+					api.ScreenToClient(hList, ptc);
+					if (PtInRect(rc, ptc)) {
 						if (Addons.HotButton.Select) {
-							Ctrl.SelectItem(Item, SVSI_SELECT | (api.GetKeyState(VK_CONTROL) < 0 ? 0 :SVSI_DESELECTOTHERS));
+							Ctrl.SelectItem(Ctrl.Item(iItem), SVSI_SELECT | (api.GetKeyState(VK_CONTROL) < 0 ? 0 :SVSI_DESELECTOTHERS));
 						}
-						var FolderItem = FolderMenu.Open(Item, pt.x, pt.y, "*", 1);
-						if (FolderItem) {
-							FolderMenu.Invoke(FolderItem);
-						}
-					}, 99)})(Ctrl);
-					return S_OK;
+						Addons.HotButton.Handled = true;
+						return S_OK;
+					}
+				}
+			} else if (Addons.HotButton.Handled && msg == WM_LBUTTONUP) {
+				var iItem = Ctrl.HitTest(pt, LVHT_ONITEM);
+				if (iItem >= 0) {
+					var rc = api.Memory("RECT");
+					Addons.HotButton.GetRect(hList, iItem, rc);
+					var ptc = pt.Clone();
+					api.ScreenToClient(hList, ptc);
+					if (PtInRect(rc, ptc)) {
+						var ptm = api.Memory("POINT");
+						ptm.x = rc.right;
+						ptm.y = rc.top;
+						api.ClientToScreen(Ctrl.hwndList, ptm);
+						(function (Item, ptm) { setTimeout(function ()
+						{
+							var FolderItem = FolderMenu.Open(Item, ptm.x, ptm.y, "*", 1);
+							if (FolderItem) {
+								FolderMenu.Invoke(FolderItem);
+							}
+						}, 99)})(Ctrl.Item(iItem), ptm);
+						return S_OK;
+					}
 				}
 			}
 		}
@@ -94,11 +114,11 @@ if (window.Addon == 1) {
 			{
 				var vm = api.SendMessage(hList, LVM_GETVIEW, 0, 0);
 				if (vm != 3) {
-					rc.Left = LVIR_SELECTBOUNDS;
+					rc.left = LVIR_SELECTBOUNDS;
 					api.SendMessage(hList, LVM_GETITEMRECT, iItem, rc);
 					return;
 				}
-				rc.Left = LVIR_ICON;
+				rc.left = LVIR_ICON;
 				api.SendMessage(hList, LVM_GETITEMRECT, iItem, rc);
 				var item = api.Memory("LVITEM");
 				item.mask = LVIF_TEXT;
@@ -106,37 +126,37 @@ if (window.Addon == 1) {
 				item.cchTextMax = 260;
 				item.iItem = iItem;
 				api.SendMessage(hList, LVM_GETITEM, 0, item);
-				rc.Right += api.SendMessage(hList, LVM_GETSTRINGWIDTH, 0, item.pszText) + 4;
+				rc.right += api.SendMessage(hList, LVM_GETSTRINGWIDTH, 0, item.pszText) + 4;
 			}
 		}
 	});
 
 	//Image
+	var w = 14 * screen.logicalYDPI / 96;
 	var s = api.PathUnquoteSpaces(ExtractMacro(te, item.getAttribute("Icon")));
 	if (s) {
-		Addons.HotButton.Image = MakeImgData(s, 0, 14) || api.CreateObject("WICBitmap").FromFile(s);
+		Addons.HotButton.Image = MakeImgData(s, 0, w) || api.CreateObject("WICBitmap").FromFile(s);
 	}
 	if (Addons.HotButton.Image) {
-		Addons.HotButton.Image = GetThumbnail(Addons.HotButton.Image, 14);
+		Addons.HotButton.Image = GetThumbnail(Addons.HotButton.Image, w);
 	} else {
 		var hdc = api.GetDC(te.hwnd);
 		var rc = api.Memory("RECT");
-		var w = 14 * screen.logicalYDPI / 96;
-		rc.Right = w;
-		rc.Bottom = w;
+		rc.right = w;
+		rc.bottom = w;
 		var hbm = api.CreateCompatibleBitmap(hdc, w, w);
 		var hmdc = api.CreateCompatibleDC(hdc);
 		var hOld = api.SelectObject(hmdc, hbm);
-		api.Rectangle(hmdc, rc.Left, rc.Top, rc.Right, rc.Bottom);
+		api.Rectangle(hmdc, rc.left, rc.top, rc.right, rc.bottom);
 		api.SetTextColor(hmdc, 0x333333);
 		api.SetBkMode(hmdc, 1);
 		var lf = api.Memory("LOGFONT");
-		lf.lfFaceName = "Arial Black",
+		lf.lfFaceName = "Consolas",
 		lf.lfHeight = - w;
 		var hFont = CreateFont(lf);
 		var hfontOld = api.SelectObject(hmdc, hFont);
-		rc.Top = -w / 4;
-		api.DrawText(hmdc, "\u25bc", -1, rc, DT_CENTER);
+		rc.top = 1 - w / 4;
+		api.DrawText(hmdc, ">", -1, rc, DT_CENTER);
 		api.SelectObject(hmdc, hfontOld);
 		api.DeleteDC(hmdc);
 		api.SelectObject(hmdc, hOld);
