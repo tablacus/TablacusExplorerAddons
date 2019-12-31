181eb015c5d2d585ea44f9a60340e46f238dd273
Gaku <rxf10242@gmail.com>
2019-12-31 22:52:25 +0900

    19.12.31

--- closewhendeleting/script.js
+++ closewhendeleting/script.js
@@ -1,13 +1,23 @@
 if (window.Addon == 1) {
 	AddEvent("ChangeNotify", function (Ctrl, pidls)
 	{
-		if (pidls.lEvent & SHCNE_RMDIR) {
+		if (pidls.lEvent & (SHCNE_RMDIR | SHCNE_UPDATEDIR)) {
 			var cFV = te.Ctrls(CTRL_FV);
 			for (var i in cFV) {
 				var FV = cFV[i];
-				if (api.ILIsParent(pidls[0], FV, false)) {
-					FV.Close();
+				if (pidls.lEvent & SHCNE_RMDIR) {
+					if (api.ILIsParent(pidls[0], FV, false)) {
+						FV.Close();
+					}
 					continue;
+				}
+				var path = api.GetDisplayNameOf(FV, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
+				if (/^[A-Z]:\\|^\\/i.test(path)) {
+					api.PathIsDirectory(function (hr, FV) {
+						if (hr < 0) {
+							FV.Close();
+						}
+					}, -1, path, FV);
 				}
 			}
 		}
