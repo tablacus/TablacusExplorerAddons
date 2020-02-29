3d170a43762488161add84fec720a0e0f61300aa
Gaku <rxf10242@gmail.com>
2020-02-29 23:24:41 +0900

    20.2.29

--- inactivepane/script.js
+++ inactivepane/script.js
@@ -6,8 +6,7 @@ if (window.Addon == 1) {
 	{
 		tid: {},
 
-		Arrange: function (Ctrl)
-		{
+		Arrange: function (Ctrl) {
 			var FV = (Ctrl.Type == CTRL_TC) ? Ctrl.Selected : Ctrl;
 			if (FV) {
 				delete Addons.InactivePane.tid[FV.hwnd];
@@ -22,8 +21,7 @@ if (window.Addon == 1) {
 			}
 		},
 
-		Arrange2: function (Ctrl)
-		{
+		Arrange2: function (Ctrl) {
 			if (Ctrl) {
 				if (Ctrl.Type == CTRL_TE) {
 					Ctrl = te.Ctrl(CTRL_FV);
@@ -34,8 +32,7 @@ if (window.Addon == 1) {
 				if (Ctrl.Type <= CTRL_EB) {
 					Addons.InactivePane.Arrange(Ctrl);
 					if (!Addons.InactivePane.tid[Ctrl.hwnd]) {
-						Addons.InactivePane.tid[Ctrl.hwnd] = setTimeout(function ()
-						{
+						Addons.InactivePane.tid[Ctrl.hwnd] = setTimeout(function () {
 							Addons.InactivePane.Arrange(Ctrl);
 						}, 500);
 					}
@@ -43,8 +40,7 @@ if (window.Addon == 1) {
 			}
 		},
 
-		SetBKColor: function (FV, hwnd, color)
-		{
+		SetBKColor: function (FV, hwnd, color) {
 			api.SendMessage(hwnd, LVM_SETBKCOLOR, 0, color);
 			api.SendMessage(hwnd, LVM_SETTEXTBKCOLOR, 0, color);
 			api.SendMessage(hwnd, LVM_SETSELECTEDCOLUMN, -1, 0);
@@ -61,8 +57,7 @@ if (window.Addon == 1) {
 			}
 		},
 
-		Active: function (FV)
-		{
+		Active: function (FV) {
 			var hwnd = FV.hwndList;
 			if (hwnd) {
 				Addons.InactivePane.SetBKColor(FV, hwnd, GetSysColor(COLOR_WINDOW));
@@ -70,47 +65,27 @@ if (window.Addon == 1) {
 			}
 		},
 
-		IsActive: function (Ctrl)
-		{
+		IsActive: function (Ctrl) {
 			return Ctrl.Parent.Id == te.Ctrl(CTRL_TC).Id;
 		},
 
-		OnChange: function (Ctrl)
-		{
+		OnChange: function (Ctrl) {
 			var cTC = te.Ctrls(CTRL_TC);
 			for (var i in cTC) {
 				var TC = cTC[i];
 				if (TC && TC.Visible) {
-					var FV = TC.Selected;
-					Addons.InactivePane.Arrange(FV);
-					var hwnd = FV.hwndList;
-					var lvbk = api.Memory("LVBKIMAGE");
-					var pszImage = api.Memory("WCHAR", 1024);
-					lvbk.pszImage = pszImage;
-					lvbk.cchImageMax = pszImage.Size;
-					lvbk.ulFlags = LVBKIF_SOURCE_URL;
-					api.SendMessage(hwnd, LVM_GETBKIMAGE, 0, lvbk);
-					if (pszImage[0]) {
-						var lvbk = api.Memory("LVBKIMAGE");
-						lvbk.ulFlags = LVBKIF_SOURCE_NONE ;
-						api.SendMessage(hwnd, LVM_SETBKIMAGE, 0, lvbk);
-					}
-					if (Addons.Stripes) {
-						Addons.Stripes.Arrange(FV);
-					}
+					Addons.InactivePane.Arrange(TC.Selected);
 				}
 			}
 		}
 	}
 
 	AddEvent("ChangeView", Addons.InactivePane.OnChange);
-	AddEvent("VisibleChanged", function ()
-	{
+	AddEvent("VisibleChanged", function () {
 		setTimeout("Addons.InactivePane.OnChange();", 99);
 	});
 
-	AddEventId("AddonDisabledEx", "inactivepane", function ()
-	{
+	AddEventId("AddonDisabledEx", "inactivepane", function () {
 		var cTC = te.Ctrls(CTRL_TC);
 		for (var i in cTC) {
 			var TC = cTC[i];
@@ -119,15 +94,20 @@ if (window.Addon == 1) {
 			}
 		}
 	});
-	var cl = item ? item.getAttribute("Color") : "";
+	var cl = item.getAttribute("Color");
 	Addons.InactivePane.Color = cl ? GetWinColor(cl) : GetSysColor(COLOR_APPWORKSPACE);
 
 	var cTC = te.Ctrls(CTRL_TC);
 	for (var i in cTC) {
 		var TC = cTC[i];
 		if (TC) {
-			var FV = TC.Selected;
-			Addons.InactivePane.Arrange2(FV);
+			Addons.InactivePane.Arrange2(TC.Selected);
 		}
+	}
+} else {
+	var ado = OpenAdodbFromTextFile("addons\\" + Addon_Id + "\\options.html");
+	if (ado) {
+		SetTabContents(0, "", ado.ReadText(adReadAll));
+		ado.Close();
 	}
 }
--- stripes/script.js
+++ stripes/script.js
@@ -1,51 +1,49 @@
-﻿var Addon_Id = "stripes";
+var Addon_Id = "stripes";
 
 var item = GetAddonElement(Addon_Id);
 if (window.Addon == 1) {
 	Addons.Stripes =
 	{
 		tid: {},
+		db: {},
 
-		Arrange: function (Ctrl, nDog, bForce)
-		{
+		Arrange: function (Ctrl, nDog) {
 			delete Addons.Stripes.tid[Ctrl.hwnd];
 			var hwnd = Ctrl.hwndList;
 			if (hwnd) {
 				var lvbk = api.Memory("LVBKIMAGE");
 				if (Ctrl.CurrentViewMode == FVM_DETAILS && !api.SendMessage(hwnd, LVM_GETGROUPCOUNT, 0, 0) && !api.SendMessage(hwnd, LVM_HASGROUP, 9425, 0)) {
-					var R = api.Memory("RECT");
-					api.SendMessage(hwnd, LVM_GETITEMRECT, 0, R);
-					var nHeight = R.Bottom - R.Top;
-					if (nHeight > 0) {
-						var pszImage = api.Memory("WCHAR", 1024);
-						lvbk.pszImage = pszImage;
-						lvbk.cchImageMax = pszImage.Size;
-						lvbk.ulFlags = LVBKIF_SOURCE_URL;
-						if (!bForce) {
-							api.SendMessage(hwnd, LVM_GETBKIMAGE, 0, lvbk);
-						}
-						if (pszImage[0] == 0) {
-							var hdc = api.GetDC(hwnd);
-							var hBitmap = api.CreateCompatibleBitmap(hdc, 1, nHeight * 2);
-							api.ReleaseDC(hwnd, hdc);
-							var image = te.WICBitmap().FromHBITMAP(hBitmap);
-							api.DeleteObject(hBitmap);
-							var cl = Addons.Stripes.GetBGRA(api.SendMessage(hwnd, LVM_GETBKCOLOR, 0, 0));
-							var cl2 = Addons.Stripes.GetBGRA(Addons.Stripes.Color);
-							for (var i = nHeight; i--;) {
-								image.SetPixel(0, i, cl);
-								image.SetPixel(0, i + nHeight, cl2);
+					var rc = api.Memory("RECT");
+					api.SendMessage(hwnd, LVM_GETITEMRECT, 0, rc);
+					var nHeight = rc.Bottom - rc.Top;
+					api.GetWindowRect(hwnd, rc);
+					var w = rc.right - rc.left, h = rc.bottom - rc.top - nHeight;
+					var hHeader = api.SendMessage(hwnd, LVM_GETHEADER, 0, 0);
+					api.GetWindowRect(hHeader, rc);
+					h -= rc.bottom - rc.top;
+					if (api.GetWindowLongPtr(hwnd, GWL_STYLE) & WS_HSCROLL) {
+						h -= api.GetSystemMetrics(SM_CYHSCROLL);
+					}
+					if (nHeight > 0 && w > 0 && h > 0) {
+						if (Addons.Stripes.db[hwnd] != h * 65536 + w) {
+							Addons.Stripes.db[hwnd] = h * 65536 + w;
+							var image = api.CreateObject("WICBitmap").Create(w, h);
+							var cl2 = Addons.Stripes.GetBGRA(Addons.Stripes.Color, Addons.Stripes.Alpha);
+							for (var i = 0; i < h; i += nHeight * 2) {
+								image.FillRect(0, i, w, nHeight, cl2);
 							}
-							lvbk.pszImage = image.DataURI("image/png");
-							lvbk.ulFlags = LVBKIF_SOURCE_URL | LVBKIF_FLAG_TILEOFFSET | LVBKIF_STYLE_TILE;
-							lvbk.yOffsetPercent = WINVER >= 0x600 ? nHeight * 2 - R.Top : 0;
-							api.SendMessage(hwnd, LVM_SETBKIMAGE, 0, lvbk);
+							lvbk.hbm = image.GetHBITMAP(-2);
+							lvbk.ulFlags = LVBKIF_TYPE_WATERMARK | LVBKIF_FLAG_ALPHABLEND;
+							if (!api.SendMessage(hwnd, LVM_SETBKIMAGE, 0, lvbk)) {
+								api.DeleteObject(lvbk.hbm);
+							}
 						}
 					} else {
 						Addons.Stripes.Retry(Ctrl, nDog);
 					}
 				} else {
-					lvbk.ulFlags = LVBKIF_SOURCE_NONE ;
+					delete Addons.Stripes.db[hwnd];
+					lvbk.ulFlags = LVBKIF_TYPE_WATERMARK;
 					api.SendMessage(hwnd, LVM_SETBKIMAGE, 0, lvbk);
 				}
 			} else {
@@ -53,8 +51,7 @@ if (window.Addon == 1) {
 			}
 		},
 
-		Arrange2: function (Ctrl)
-		{
+		Arrange2: function (Ctrl) {
 			if (Ctrl) {
 				if (Ctrl.Type == CTRL_TE) {
 					Ctrl = te.Ctrl(CTRL_FV);
@@ -62,11 +59,10 @@ if (window.Addon == 1) {
 						return;
 					}
 				}
-				if (Ctrl.Type <= CTRL_EB) {
+				if (Ctrl.Type == CTRL_SB) {
 					Addons.Stripes.Arrange(Ctrl);
 					if (!Addons.Stripes.tid[Ctrl.hwnd]) {
-						Addons.Stripes.tid[Ctrl.hwnd] = setTimeout(function ()
-						{
+						Addons.Stripes.tid[Ctrl.hwnd] = setTimeout(function () {
 							Addons.Stripes.Arrange(Ctrl);
 						}, 99);
 					}
@@ -74,59 +70,68 @@ if (window.Addon == 1) {
 			}
 		},
 
-		GetBGRA: function (c)
-		{
-			return ((c & 0xff) << 16) | (c & 0xff00) | ((c & 0xff0000) >> 16) | 0xff000000;
+		Arrange3: function (Ctrl) {
+			setTimeout(function () {
+				var cTC = te.Ctrls(CTRL_TC);
+				for (var i in cTC) {
+					var TC = cTC[i];
+					if (TC && TC.Visible) {
+						Addons.Stripes.Arrange(TC.Selected);
+					}
+				}
+			}, 99);
 		},
 
-		SizeChanged: function (Ctrl)
-		{
-			Addons.Stripes.Arrange(Ctrl, 0, true);
+		GetBGRA: function (c, a) {
+			return ((c & 0xff) << 16) | (c & 0xff00) | ((c & 0xff0000) >> 16) | a << 24;
 		},
 
-		Retry: function (Ctrl, nDog)
-		{
+		Retry: function (Ctrl, nDog) {
 			nDog = (nDog || 0) + 1;
 			if (nDog < 9) {
 				Addons.Stripes.tid[Ctrl.hwnd] = setTimeout(function () {
 					Addons.Stripes.Arrange(Ctrl, nDog);
 				}, nDog * 100);
 			}
-		}
-	}
+		},
 
-	Addons.Stripes.Color = GetWinColor(item.getAttribute("Color2") || "#ececec");
-	AddEvent("NavigateComplete", Addons.Stripes.Arrange);
-	AddEvent("Command", Addons.Stripes.Arrange2);
-	AddEvent("IconSizeChanged", Addons.Stripes.SizeChanged);
-	AddEvent("InvokeCommand", function (ContextMenu, fMask, hwnd, Verb, Parameters, Directory, nShow, dwHotKey, hIcon)
-	{
-		Addons.Stripes.Arrange2(te.CtrlFromWindow(hwnd));
-	});
+		Resize: function (tm) {
+			setTimeout(function () {
+				var cFV = te.Ctrls(CTRL_FV, false);
+				for (var i in cFV) {
+					var hwnd = cFV[i].hwndList;
+					if (hwnd) {
+						Addons.Stripes.Arrange(cFV[i]);
+					}
+				}
+			}, api.LowPart(tm) || 99);
+		},
 
-	AddEventId("AddonDisabledEx", "stripes", function()
-	{
-		var cFV = te.Ctrls(CTRL_FV);
-		var lvbk = api.Memory("LVBKIMAGE");
-		lvbk.ulFlags = LVBKIF_SOURCE_NONE ;
-		for (var i in cFV) {
-			var hwnd = cFV[i].hwndList;
-			if (hwnd) {
+		Clear: function () {
+			var lvbk = api.Memory("LVBKIMAGE");
+			lvbk.ulFlags = LVBKIF_TYPE_WATERMARK;
+			for (var hwnd in Addons.Stripes.db) {
+				delete Addons.Stripes.db[hwnd];
 				api.SendMessage(hwnd, LVM_SETBKIMAGE, 0, lvbk);
 			}
 		}
-	});
-
-	var cFV = te.Ctrls(CTRL_FV);
-	var lvbk = api.Memory("LVBKIMAGE");
-	lvbk.ulFlags = LVBKIF_SOURCE_NONE ;
-	for (var i in cFV) {
-		var hwnd = cFV[i].hwndList;
-		if (hwnd) {
-			api.SendMessage(hwnd, LVM_SETBKIMAGE, 0, lvbk);
-			Addons.Stripes.Arrange2(cFV[i]);
-		}
 	}
+
+	Addons.Stripes.Color = GetWinColor(item.getAttribute("Color2") || "#cccccc");
+	Addons.Stripes.Alpha = (item.getAttribute("Alpha") & 0xff) || 128;
+	AddEvent("NavigateComplete", Addons.Stripes.Arrange);
+	AddEvent("Command", Addons.Stripes.Arrange2);
+	AddEvent("IconSizeChanged", Addons.Stripes.Arrange);
+	AddEvent("Arrange", Addons.Stripes.Arrange3);
+	AddEvent("InvokeCommand", function (ContextMenu, fMask, hwnd, Verb, Parameters, Directory, nShow, dwHotKey, hIcon) {
+		Addons.Stripes.Arrange2(te.CtrlFromWindow(hwnd));
+	});
+	AddEventId("AddonDisabledEx", "stripes", Addons.Stripes.Clear);
+	Addons.Stripes.Resize(5000);
 } else {
-	SetTabContents(0, "Color", '<input type="text" id="Color2" style="width: 7em" onchange="ChangeColor1(this)" /><input id="Color_Color2" type="button" value=" " class="color" onclick="ChooseColor2(this)" />');
+	var ado = OpenAdodbFromTextFile("addons\\" + Addon_Id + "\\options.html");
+	if (ado) {
+		SetTabContents(0, "", ado.ReadText(adReadAll));
+		ado.Close();
+	}
 }
