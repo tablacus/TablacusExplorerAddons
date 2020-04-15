c7b50152f409bf57863663032efc5a890deebe13
Gaku <rxf10242@gmail.com>
2020-04-15 22:57:29 +0900

    20.4.15

--- cbcopy/script.js
+++ cbcopy/script.js
@@ -1,19 +1,18 @@
 if (window.Addon == 1) {
 	Addons.CBCopy =
 	{
-		Copy: function (Items)
-		{
+		Copy: function (Items) {
 			var pdwEffect = Items.pdwEffect;
 			if (pdwEffect) {
 				pdwEffect[0] = DROPEFFECT_COPY | DROPEFFECT_LINK;
 			}
+			Items.UseText = true;
 			api.OleSetClipboard(Items);
 			return S_OK;
 		}
 	}
 
-	AddEvent("Command", function (Ctrl, hwnd, msg, wParam, lParam)
-	{
+	AddEvent("Command", function (Ctrl, hwnd, msg, wParam, lParam) {
 		if (Ctrl.Type == CTRL_SB || Ctrl.Type == CTRL_EB) {
 			if ((wParam & 0xfff) == CommandID_COPY - 1) {
 				return Addons.CBCopy.Copy(Ctrl.SelectedItems);
@@ -21,8 +20,7 @@ if (window.Addon == 1) {
 		}
 	});
 
-	AddEvent("InvokeCommand", function (ContextMenu, fMask, hwnd, Verb, Parameters, Directory, nShow, dwHotKey, hIcon)
-	{
+	AddEvent("InvokeCommand", function (ContextMenu, fMask, hwnd, Verb, Parameters, Directory, nShow, dwHotKey, hIcon) {
 		if (Verb == CommandID_COPY - 1) {
 			return Addons.CBCopy.Copy(ContextMenu.Items);
 		}
