07499347e16f404ae40a20d5c86c4c344f13379a
Gaku <rxf10242@gmail.com>
2020-06-20 23:10:13 +0900

    20.6.20

--- fixselection/script.js
+++ fixselection/script.js
@@ -2,47 +2,42 @@
 	Addons.FixSelection = {
 		tid: {},
 
-		Exec: function (FV)
-		{
+		Exec: function (FV) {
 			delete Addons.FixSelection.tid[FV.Id];
 			if (FV.Type == CTRL_SB && FV.Data) {
 				var Selected = FV.Data.Selected;
 				if (Selected && Selected.Count != FV.ItemCount(SVGIO_SELECTION)) {
-					FV.SelectItem(null, SVSI_DESELECTOTHERS);
-					for (var i = Selected.Count; i--;) {
-						FV.SelectItem(Selected.Item(i), SVSI_SELECT);
+					var wFlags = SVSI_DESELECTOTHERS;
+					for (var i = 0; i < Selected.Count; ++i) {
+						FV.SelectItem(Selected.Item(i), SVSI_SELECT | wFlags);
+						wFlags = 0;
 					}
 					delete FV.Data.Selected;
 				}
 			}
 		},
 
-		ChangeNotify: function (FV)
-		{
+		ChangeNotify: function (FV) {
 			if (FV.Type == CTRL_SB && FV.Data && FV.Data.Selected) {
 				if (Addons.FixSelection.tid[FV.Id]) {
 					clearTimeout(Addons.FixSelection.tid[FV.Id]);
 				}
-				Addons.FixSelection.tid[FV.Id] = setTimeout(function ()
-				{
+				Addons.FixSelection.tid[FV.Id] = setTimeout(function () {
 					Addons.FixSelection.Exec(FV);
 				}, 99)
 			}
 		}
 	}
 
-	AddEvent("BeforeNavigate", function (Ctrl, fs, wFlags, Prev)
-	{
+	AddEvent("BeforeNavigate", function (Ctrl, fs, wFlags, Prev) {
 		Ctrl.Data.Selected = null;
 	});
 
-	AddEvent("SelectionChanged", function (Ctrl, uChange)
-	{
-		if (Ctrl.Type == CTRL_SB && Ctrl.Data) {
+	AddEvent("SelectionChanged", function (Ctrl, uChange) {
+		if (Ctrl.Type == CTRL_SB && Ctrl.Data && !Addons.FixSelection.Refresh) {
 			var Selected = Ctrl.SelectedItems();
 			if (Selected.Count == 0) {
-				setTimeout(function ()
-				{
+				setTimeout(function () {
 					if (Ctrl.Data) {
 						Ctrl.Data.Selected = Ctrl.SelectedItems();
 					}
@@ -55,8 +50,31 @@
 
 	AddEvent("Sort", Addons.FixSelection.Exec);
 
-	AddEvent("ChangeNotify", function (Ctrl, pidls, wParam, lParam)
-	{
+	AddEvent("Command", function (Ctrl, hwnd, msg, wParam, lParam) {
+		if (msg == WM_NULL) {
+			Addons.FixSelection.Refresh = Ctrl.Data.Selected;
+		}
+	});
+
+	AddEvent("NavigateComplete", function (Ctrl) {
+		if (Addons.FixSelection.Refresh) {
+			(function (Selected) {
+				setTimeout(function () {
+					delete Addons.FixSelection.Refresh;
+					if (Selected && Selected.Count != FV.ItemCount(SVGIO_SELECTION)) {
+						var wFlags = SVSI_FOCUSED | SVSI_DESELECTOTHERS;
+						for (var i = 0; i < Selected.Count; ++i) {
+							FV.SelectItem(Selected.Item(i), SVSI_SELECT | wFlags);
+							wFlags = 0;
+						}
+					}
+				}, 99);
+			})(Addons.FixSelection.Refresh);
+			delete Addons.FixSelection.Refresh;
+		}
+	});
+
+	AddEvent("ChangeNotify", function (Ctrl, pidls, wParam, lParam) {
 		var cFV = te.Ctrls(CTRL_FV);
 		for (var i in cFV) {
 			Addons.FixSelection.ChangeNotify(cFV[i]);
