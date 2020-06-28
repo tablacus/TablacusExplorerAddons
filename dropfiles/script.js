829c19c029be0254fcd44494435d771aef9847fa
Gaku <rxf10242@gmail.com>
2020-06-28 21:28:24 +0900

    20.6.28

--- dropfiles/script.js
+++ dropfiles/script.js
@@ -1,23 +1,35 @@
 if (window.Addon == 1) {
-	AddType("Drop files",
-	{
-		Exec: function (Ctrl, s, type, hwnd, pt)
-		{
+	Addons.DropFiles = {
+		db: [],
+
+		DoDrop: function () {
+			var db = Addons.DropFiles.db;
+			for (i = db.length; i--;) {
+				var hwnd = GethwndFromPid(db[i].Exec.ProcessID, 1);
+				if (hwnd) {
+					api.PostMessage(hwnd, WM_DROPFILES, db[i].Selected.hDrop, 0);
+				}
+				if (hwnd || --db[i].Retry < 0) {
+					db.splice(i, 1);
+				}
+			}
+			if (db.length) {
+				setTimeout(Addons.DropFiles.DoDrop, 999);
+			}
+		}
+	};
+
+	AddType("Drop files", {
+		Exec: function (Ctrl, s, type, hwnd, pt) {
 			var FV = GetFolderView(Ctrl, pt);
 			var Selected = FV.SelectedItems();
 			if (Selected && Selected.Count) {
-				try {
-					var path = api.PathUnquoteSpaces(ExtractMacro(te, s));
-					var oExec = wsh.Exec(path);
-					var hwnd = GethwndFromPid(oExec.ProcessID, 1);
-					if (hwnd) {
-						api.PostMessage(hwnd, WM_DROPFILES, Selected.hDrop, 0);
-					} else {
-						MessageBox("Can't drop.", TITLE, MB_ICONSTOP | MB_OK);
-					}
-				} catch (e) {
-					MessageBox([e.description || e.toString(), path].join("\n\n"), TITLE, MB_OK);
-				}
+				Addons.DropFiles.db.push({
+					Exec: wsh.Exec(api.PathUnquoteSpaces(ExtractMacro(te, s))),
+					Selected: Selected,
+					Retry: 9
+				});
+				Addons.DropFiles.DoDrop();
 			}
 			return S_OK;
 		},
--- emulatedrop/script.js
+++ emulatedrop/script.js
@@ -1,36 +1,53 @@
 if (window.Addon == 1) {
+	Addons.EmulateDrop = {
+		db: [],
+
+		DoDrop: function () {
+			var db = Addons.EmulateDrop.db;
+			for (i = db.length; i--;) {
+				var hwnd = GethwndFromPid(db[i].Exec.ProcessID, 1);
+				if (hwnd) {
+					var rc = api.Memory("RECT");
+					api.GetWindowRect(hwnd, rc);
+					var pt = api.Memory("POINT"), pt1 = api.Memory("POINT");
+					pt1.x = rc.left + 16;
+					pt1.y = rc.top + 8;
+					api.SetForegroundWindow(hwnd);
+					if (hwnd != api.WindowFromPoint(pt1) || api.GetKeyState(VK_LBUTTON) < 0 || api.GetKeyState(VK_RBUTTON) < 0) {
+						hwnd = 0;
+					} else {
+						api.GetCursorPos(pt);
+						api.SetCursorPos(pt1.x, pt1.y);
+						var tid = setTimeout(function () {
+							api.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+						}, 99);
+						api.SHDoDragDrop(null, db[i].Selected, te, DROPEFFECT_COPY | DROPEFFECT_MOVE | DROPEFFECT_LINK, db[i].Selected.pdwEffect);
+						clearTimeout(tid);
+						api.SetCursorPos(pt.x, pt.y);
+					}
+				}
+				if (hwnd || --db[i].Retry < 0) {
+					db.splice(i, 1);
+					break;
+				}
+			}
+			if (db.length) {
+				setTimeout(Addons.EmulateDrop.DoDrop, 999);
+			}
+		}
+	};
+
 	AddType("Emulate drop", {
 		Exec: function (Ctrl, s, type, hwnd, pt) {
 			var FV = GetFolderView(Ctrl, pt);
 			var Selected = FV.SelectedItems();
 			if (Selected && Selected.Count) {
-				try {
-					var path = api.PathUnquoteSpaces(ExtractMacro(te, s));
-					var oExec = wsh.Exec(path);
-					var hwnd = GethwndFromPid(oExec.ProcessID, 3);
-					if (hwnd) {
-						while (api.GetASyncKeyState(VK_LBUTTON) < 0 || api.GetASyncKeyState(VK_RBUTTON) < 0) {
-							api.Sleep(999);
-						}
-						var rc = api.Memory("RECT");
-						api.GetWindowRect(hwnd, rc);
-						var pt = api.Memory("POINT"), pt1 = api.Memory("POINT");
-						pt1.x = rc.left + 16;
-						pt1.y = rc.top + 8;
-						for (var nDog = 60; nDog-- && hwnd != api.WindowFromPoint(pt1);) {
-							api.Sleep(999);
-							api.SetForegroundWindow(hwnd);
-						}
-						if (nDog > 0) {
-							api.GetCursorPos(pt);
-							api.SetCursorPos(pt1.x, pt1.y);
-							api.SHDoDragDrop(null, Selected, te, DROPEFFECT_COPY | DROPEFFECT_MOVE | DROPEFFECT_LINK, Selected.pdwEffect);
-							api.SetCursorPos(pt.x, pt.y);
-						}
-					}
-				} catch (e) {
-					MessageBox([e.description || e.toString(), path].join("\n\n"), TITLE, MB_OK);
-				}
+				Addons.EmulateDrop.db.push({
+					Exec: wsh.Exec(api.PathUnquoteSpaces(ExtractMacro(te, s))),
+					Selected: Selected,
+					Retry: 9
+				});
+				Addons.EmulateDrop.DoDrop();
 			}
 			return S_OK;
 		},
