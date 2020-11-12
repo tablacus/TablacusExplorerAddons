71b79878f715be4cb2291ff8d6124318e12be397
Gaku <rxf10242@gmail.com>
2020-11-12 23:27:02 +0900

    20.11.12

--- usercss/options.js
+++ usercss/options.js
@@ -1,39 +1,35 @@
 SetTabContents(4, "CSS", '<form name="E" style=" height: 100%"><textarea name="css" style="width: 100%; height: 100%" onchange="g_bCss=true"></textarea></form>');
-document.getElementById("toolbar").innerHTML = '<input type="button" value="Refresh" onclick="ReloadUserCss()" />&nbsp;<input type="button" value="Open" onclick="EditUserCss()" />';
+document.getElementById("toolbar").innerHTML = '<input type="button" value="Refresh" onclick="ReloadUserCss()" />&nbsp;<input type="button" value="Open" onclick="EditUserCss()">';
 
-ReadUserCss = function ()
+ReadUserCss = async function ()
 {
 	g_bCss = false;
-	var ado = OpenAdodbFromTextFile(fso.BuildPath(te.Data.DataFolder, "config\\user.css"));
-	if (ado) {
-		document.E.css.value = ado.ReadText(adReadAll);
-		ado.Close();
-	}
+	document.E.css.value = await ReadTextFile(BuildPath(await te.Data.DataFolder, "config\\user.css")) || "";
 }
 
-ReloadUserCss = function ()
+ReloadUserCss = async function ()
 {
-	if (!window.g_bCss || confirmOk("Are you sure?")) {
+	if (!window.g_bCss || await confirmOk()) {
 		ReadUserCss();
 	}
 }
 
-EditUserCss = function ()
+EditUserCss = async function ()
 {
 	SaveUserCss();
-	MainWindow.InvokeCommand(fso.BuildPath(te.Data.DataFolder, "config\\user.css"), 0, te.hwnd, "Edit", null, null, SW_SHOWNORMAL, 0, 0, te, CMF_DEFAULTONLY);
+	MainWindow.InvokeCommand(BuildPath(await te.Data.DataFolder, "config\\user.css"), 0, await te.hwnd, "Edit", null, null, SW_SHOWNORMAL, 0, 0, te, CMF_DEFAULTONLY);
 }
 
-SaveUserCss = function ()
+SaveUserCss = async function ()
 {
 	if (window.g_bCss) {
-		var fn = fso.BuildPath(te.Data.DataFolder, "config\\user.css");
+		var fn = BuildPath(await te.Data.DataFolder, "config\\user.css");
 		try {
-			var ado = api.CreateObject("ads");
+			var ado = await api.CreateObject("ads");
 			ado.CharSet = "utf-8";
-			ado.Open();
-			ado.WriteText(document.E.css.value.replace(/\r\n/g, "\n").replace(/\n/g, "\r\n"));
-			ado.SaveToFile(fn, adSaveCreateOverWrite);
+			await ado.Open();
+			await ado.WriteText(document.E.css.value.replace(/\r\n/g, "\n").replace(/\n/g, "\r\n"));
+			await ado.SaveToFile(fn, adSaveCreateOverWrite);
 			ado.Close();
 			g_bCss = false;
 		} catch (e) {
@@ -43,4 +39,4 @@ SaveUserCss = function ()
 }
 
 SaveLocation = SaveUserCss;
-ReadUserCss();
+setTimeout(ReadUserCss, 99);
--- usercss/script.js
+++ usercss/script.js
@@ -1,12 +1,12 @@
 var Addons_Id = "usercss";
 if (window.Addon == 1) {
-	AddEvent("BrowserCreated", function (doc) {
-		var link = doc.createElement("link");
+	AddEvent("BrowserCreatedEx", async function () {
+		var link = document.createElement("link");
 		link.rel = "stylesheet";
 		link.type = "text/css";
-		link.href = api.UrlCreateFromPath(fso.BuildPath(te.Data.DataFolder, "config\\user.css"));
-		doc.head.appendChild(link);
-	});
+		link.href = await api.UrlCreateFromPath(BuildPath(await te.Data.DataFolder, "config\\user.css"));
+		document.head.appendChild(link);
+	}.toString().replace(/^[^{]+{|}$/g, ""));
 } else {
 	importScript("addons\\" + Addon_Id + "\\options.js");
 }
