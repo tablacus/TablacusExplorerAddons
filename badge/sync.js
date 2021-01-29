94badbd7c417b9f062b8d90698b112b11d8f8ba7
Gaku <rxf10242@gmail.com>
2021-01-29 21:40:13 +0900

    21.1.29

--- badge/sync.js
+++ badge/sync.js
@@ -1,9 +1,8 @@
-var Addon_Id = "badge";
-var item = GetAddonElement(Addon_Id);
+const Addon_Id = "badge";
+const item = GetAddonElement(Addon_Id);
 
 Sync.Badge = {
 	RE: /badge:(.*)/i,
-	CONFIG: fso.BuildPath(te.Data.DataFolder, "config\\badge.tsv"),
 	strName: item.getAttribute("MenuName") || GetAddonInfo(Addon_Id).Name,
 	nPos: GetNum(item.getAttribute("MenuPos")),
 	Changed: {},
@@ -24,9 +23,9 @@ Sync.Badge = {
 	},
 
 	SetX: function (Ctrl, pt, s) {
-		var Selected = GetSelectedArray(Ctrl, pt, true).shift();
+		const Selected = GetSelectedArray(Ctrl, pt, true).shift();
 		if (Selected && Selected.Count) {
-			for (var i = Selected.Count; i-- > 0;) {
+			for (let i = Selected.Count; i-- > 0;) {
 				Sync.Badge.Set(api.GetDisplayNameOf(Selected.Item(i), SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL), s);
 			}
 		}
@@ -81,10 +80,10 @@ Sync.Badge = {
 	},
 
 	Enum: function (pid, Ctrl, fncb, SessionId) {
-		var Items = te.FolderItems();
-		var Badge = Sync.Badge.BadgePath(pid);
+		const Items = te.FolderItems();
+		const Badge = Sync.Badge.BadgePath(pid);
 		Sync.Badge.DB.ENumCB(function (path, s) {
-			var parent = fso.GetParentFolderName(path);
+			const parent = GetParentFolderName(path);
 			if (api.PathMatchSpec(Badge, s) || api.PathMatchSpec(parent, Badge)) {
 				Items.AddItem(path);
 			}
@@ -93,31 +92,32 @@ Sync.Badge = {
 	},
 
 	EditPath: function (Ctrl, pt) {
-		var Selected = GetSelectedArray(Ctrl, pt, true).shift();
+		const Selected = GetSelectedArray(Ctrl, pt, true).shift();
 		if (Selected && Selected.Count == 1) {
 			try {
-				var path = api.GetDisplayNameOf(Selected.Item(0), SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
+				const path = api.GetDisplayNameOf(Selected.Item(0), SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
 				if (path) {
-					var Badge = Sync.Badge.DB.Get(path);
-					var s = InputDialog("badge:" + Badge + "\n" + path, path);
-					if ("string" === typeof s) {
-						api.SHParseDisplayName(function (pid, s, path, Badge) {
-							if (pid) {
-								s = api.GetDisplayNameOf(pid, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
-							}
-							if (s != path) {
-								if (Sync.Badge.DB.Get(s)) {
-									wsh.Popup(api.LoadString(hShell32, 6327));
-								} else {
-									Sync.Badge.Set(s, Badge);
-									Sync.Badge.Set(path, "");
+					const Badge = Sync.Badge.DB.Get(path);
+					InputDialog("badge:" + Badge + "\n" + path, path, function (s) {
+						if ("string" === typeof s) {
+							api.SHParseDisplayName(function (pid, s, path, Badge) {
+								if (pid) {
+									s = api.GetDisplayNameOf(pid, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
 								}
-							}
-						}, 0, s, s, path, Badge);
-					}
+								if (s != path) {
+									if (Sync.Badge.DB.Get(s)) {
+										wsh.Popup(api.LoadString(hShell32, 6327));
+									} else {
+										Sync.Badge.Set(s, Badge);
+										Sync.Badge.Set(path, "");
+									}
+								}
+							}, 0, s, s, path, Badge);
+						}
+					});
 				}
 			} catch (e) {
-				wsh.Popup(e.description + "\n" + s, 0, TITLE, MB_ICONSTOP);
+				wsh.Popup(e.message + "\n" + s, 0, TITLE, MB_ICONSTOP);
 			}
 		}
 	},
@@ -126,18 +126,18 @@ Sync.Badge = {
 		if (!confirmOk()) {
 			return;
 		}
-		var ar = GetSelectedArray(Ctrl, pt);
-		var Selected = ar.shift();
-		var SelItem = ar.shift();
-		var FV = ar.shift();
-		var Badge = Sync.Badge.BadgePath(FV);
+		const ar = GetSelectedArray(Ctrl, pt);
+		const Selected = ar.shift();
+		const SelItem = ar.shift();
+		const FV = ar.shift();
+		const Badge = Sync.Badge.BadgePath(FV);
 		if (Badge) {
 			Sync.Badge.RemoveItems(Selected, Badge);
 		}
 	},
 
 	BadgePath: function (Ctrl) {
-		var res = Sync.Badge.IsHandle(Ctrl);
+		const res = Sync.Badge.IsHandle(Ctrl);
 		if (res) {
 			return res[1];
 		}
@@ -147,7 +147,7 @@ Sync.Badge = {
 		if (!confirmOk()) {
 			return;
 		}
-		var FV = GetFolderView(Ctrl, pt);
+		const FV = GetFolderView(Ctrl, pt);
 		if (FV) {
 			Selected = FV.SelectedItems();
 			Sync.Badge.AppendItems(Selected, ExtraMenuData[nVerb]);
@@ -158,7 +158,7 @@ Sync.Badge = {
 		if (!confirmOk()) {
 			return;
 		}
-		var FV = GetFolderView(Ctrl, pt);
+		const FV = GetFolderView(Ctrl, pt);
 		if (FV) {
 			Selected = FV.SelectedItems();
 			Sync.Badge.RemoveItems(Selected, ExtraMenuData[nVerb]);
@@ -167,7 +167,7 @@ Sync.Badge = {
 
 	AppendItems: function (Items, Badge) {
 		if (Items) {
-			for (var i = 0; i < Items.Count; i++) {
+			for (let i = 0; i < Items.Count; i++) {
 				Sync.Badge.Set(Items.Item(i), Badge);
 			}
 		}
@@ -175,15 +175,15 @@ Sync.Badge = {
 
 	RemoveItems: function (Items) {
 		if (Items) {
-			for (var i = Items.Count; i-- > 0;) {
+			for (let i = Items.Count; i-- > 0;) {
 				Sync.Badge.Remove(Items.Item(i));
 			}
 		}
 	},
 
 	Remove: function (Item) {
-		var s = "";
-		var path = api.GetDisplayNameOf(Item, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
+		let s = "";
+		const path = api.GetDisplayNameOf(Item, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
 		if (path) {
 			s = Sync.Badge.DB.Get(path);
 			Sync.Badge.Set(path, 0);
@@ -201,8 +201,8 @@ Sync.Badge = {
 	},
 
 	Paste: function (Ctrl, pt) {
-		var FV = GetFolderView(Ctrl, pt);
-		var Badge = Sync.Badge.BadgePath(FV);
+		const FV = GetFolderView(Ctrl, pt);
+		const Badge = Sync.Badge.BadgePath(FV);
 		if (GetNum(Badge)) {
 			Sync.Badge.AppendItems(api.OleGetClipboard(), Badge);
 		}
@@ -210,8 +210,8 @@ Sync.Badge = {
 	},
 
 	PasteEx: function (Ctrl, pt) {
-		var FV = GetFolderView(Ctrl, pt);
-		var Selected = FV.SelectedItems();
+		const FV = GetFolderView(Ctrl, pt);
+		const Selected = FV.SelectedItems();
 		if (!Selected.Count || !Selected.Item(0).IsFolder) {
 			return Sync.Badge.Paste(FV);
 		}
@@ -219,18 +219,18 @@ Sync.Badge = {
 
 
 	Notify: function () {
-		var cFV = te.Ctrls(CTRL_FV);
-		for (var i in cFV) {
-			var FV = cFV[i];
+		const cFV = te.Ctrls(CTRL_FV);
+		for (let i in cFV) {
+			const FV = cFV[i];
 			if (FV.hwnd) {
-				var path = api.GetDisplayNameOf(FV, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
+				const path = api.GetDisplayNameOf(FV, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
 				if (Sync.Badge.Redraw[path]) {
 					api.InvalidateRect(FV.hwndList || FV.hwndView || FV.hwnd, null, false);
 				}
-				var s = Sync.Badge.BadgePath(FV);
+				const s = Sync.Badge.BadgePath(FV);
 				if (s) {
-					var b = false;
-					for (var j in Sync.Badge.Changed) {
+					let b = false;
+					for (let j in Sync.Badge.Changed) {
 						if (b = api.PathMatchSpec(j, s)) {
 							break;
 						}
@@ -247,16 +247,16 @@ Sync.Badge = {
 	},
 
 	List: function (list) {
-		var ix = [];
+		let ix = [];
 		Sync.Badge.DB.ENumCB(function (path, s) {
 			if (s) {
 				ix.push(s);
 			}
 		});
-		var ix = ix.sort(function (a, b) {
+		ix = ix.sort(function (a, b) {
 			return api.StrCmpLogical(b, a)
 		});
-		for (var i = ix.length; i--;) {
+		for (let i = ix.length; i--;) {
 			list[ix[i]] = true;
 		}
 	},
@@ -300,19 +300,18 @@ AddEvent("Load", function () {
 	Sync.Badge.DB.OnChange = function (n, s, old) {
 		clearTimeout(Sync.Badge.tid2);
 		Sync.Badge.tid2 = setTimeout(Sync.Badge.Notify, 500);
-		Sync.Badge.Redraw[fso.GetParentFolderName(n)] = true;
+		Sync.Badge.Redraw[GetParentFolderName(n)] = true;
 		Sync.Badge.Changed[s] = true;
 		Sync.Badge.Changed[old] = true;
 	}
 	AddEvent("SaveConfig", Sync.Badge.DB.Save);
 	AddEvent("Finalize", Sync.Badge.DB.Close);
 
-	var Installed0 = Sync.Badge.DB.Get('%Installed%').toUpperCase();
-	var Installed0 = Sync.Badge.Set('%Installed%', "");
-	var Installed1 = Sync.Badge.Portable ? fso.GetDriveName(api.GetModuleFileName(null)).toUpperCase() : "";
+	const Installed0 = Sync.Badge.DB.Get('%Installed%').toUpperCase();
+	const Installed1 = Sync.Badge.Portable ? fso.GetDriveName(api.GetModuleFileName(null)).toUpperCase() : "";
 	if (Installed0 && Sync.Badge.Portable && Installed0 != Installed1) {
 		Sync.Badge.DB.ENumCB(function (path, badge) {
-			var drv = fso.GetDriveName(path);
+			const drv = fso.GetDriveName(path);
 			if (drv.toUpperCase() == Installed0) {
 				Sync.Badge.Set(path, "");
 				Sync.Badge.Set(Installed1 + path.substr(drv.length), badge);
@@ -332,7 +331,7 @@ AddEvent("TranslatePath", function (Ctrl, Path) {
 }, true);
 
 AddEvent("GetFolderItemName", function (pid) {
-	var Badge = Sync.Badge.BadgePath(pid);
+	const Badge = Sync.Badge.BadgePath(pid);
 	if (Badge) {
 		return "badge:" + Badge;
 	}
@@ -362,22 +361,22 @@ AddEvent("Command", function (Ctrl, hwnd, msg, wParam, lParam) {
 
 AddEvent("InvokeCommand", function (ContextMenu, fMask, hwnd, Verb, Parameters, Directory, nShow, dwHotKey, hIcon) {
 	if (Verb == CommandID_DELETE - 1) {
-		var FV = ContextMenu.FolderView;
+		const FV = ContextMenu.FolderView;
 		if (FV && Sync.Badge.IsHandle(FV)) {
 			return S_OK;
 		}
 	}
 	if (Verb == CommandID_PASTE - 1) {
-		var FV = ContextMenu.FolderView;
+		const FV = ContextMenu.FolderView;
 		if (FV && Sync.Badge.IsHandle(FV)) {
 			return Sync.Badge.PasteEx(Ctrl);
 		}
 	}
 	if (!Verb || Verb == CommandID_STORE - 1) {
 		if (ContextMenu.Items.Count >= 1) {
-			var path = api.GetDisplayNameOf(ContextMenu.Items.Item(0), SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
+			const path = api.GetDisplayNameOf(ContextMenu.Items.Item(0), SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
 			if (Sync.Badge.IsHandle(path)) {
-				var FV = te.Ctrl(CTRL_FV);
+				const FV = te.Ctrl(CTRL_FV);
 				FV.Navigate(path, SBSP_SAMEBROWSER);
 				return S_OK;
 			}
@@ -387,7 +386,7 @@ AddEvent("InvokeCommand", function (ContextMenu, fMask, hwnd, Verb, Parameters, 
 
 AddEvent("DragEnter", function (Ctrl, dataObj, grfKeyState, pt, pdwEffect) {
 	if (Ctrl.Type <= CTRL_EB || Ctrl.Type == CTRL_DT) {
-		var Badge = Sync.Badge.BadgePath(Ctrl);
+		const Badge = Sync.Badge.BadgePath(Ctrl);
 		if (GetNum(Badge)) {
 			return S_OK;
 		}
@@ -396,7 +395,7 @@ AddEvent("DragEnter", function (Ctrl, dataObj, grfKeyState, pt, pdwEffect) {
 
 AddEvent("DragOver", function (Ctrl, dataObj, grfKeyState, pt, pdwEffect) {
 	if (Ctrl.Type <= CTRL_EB) {
-		var Badge = Sync.Badge.BadgePath(Ctrl);
+		const Badge = Sync.Badge.BadgePath(Ctrl);
 		if (GetNum(Badge)) {
 			if (Ctrl.HitTest(pt, LVHT_ONITEM) < 0) {
 				pdwEffect[0] = DROPEFFECT_LINK;
@@ -405,7 +404,7 @@ AddEvent("DragOver", function (Ctrl, dataObj, grfKeyState, pt, pdwEffect) {
 		}
 	}
 	if (Ctrl.Type == CTRL_DT) {
-		var Badge = Sync.Badge.BadgePath(Ctrl);
+		const Badge = Sync.Badge.BadgePath(Ctrl);
 		if (GetNum(Badge)) {
 			pdwEffect[0] = DROPEFFECT_LINK;
 			return S_OK;
@@ -414,9 +413,9 @@ AddEvent("DragOver", function (Ctrl, dataObj, grfKeyState, pt, pdwEffect) {
 }, true);
 
 AddEvent("Drop", function (Ctrl, dataObj, grfKeyState, pt, pdwEffect) {
-	var Badge = Sync.Badge.BadgePath(Ctrl);
+	const Badge = Sync.Badge.BadgePath(Ctrl);
 	if (GetNum(Badge)) {
-		var nIndex = -1;
+		let nIndex = -1;
 		if (Ctrl.Type <= CTRL_EB) {
 			nIndex = Ctrl.HitTest(pt, LVHT_ONITEM);
 		} else if (Ctrl.Type != CTRL_DT) {
@@ -435,18 +434,18 @@ AddEvent("DragLeave", function (Ctrl) {
 
 AddEvent("Menus", function (Ctrl, hMenu, nPos, Selected, SelItem, ContextMenu, Name, pt) {
 	if (/Background|Edit/i.test(Name)) {
-		var Badge = Sync.Badge.BadgePath(GetFolderView(Ctrl, pt));
+		const Badge = Sync.Badge.BadgePath(GetFolderView(Ctrl, pt));
 		if (GetNum(Badge)) {
-			var Items = api.OleGetClipboard();
+			const Items = api.OleGetClipboard();
 			if (Items && Items.Count) {
-				var mii = api.Memory("MENUITEMINFO");
+				const mii = api.Memory("MENUITEMINFO");
 				mii.cbSize = mii.Size;
 				mii.fMask = MIIM_ID | MIIM_STATE;
-				var paste = api.LoadString(hShell32, 33562) || "&Paste";
-				for (var i = api.GetMenuItemCount(hMenu); i-- > 0;) {
+				const paste = api.LoadString(hShell32, 33562) || "&Paste";
+				for (let i = api.GetMenuItemCount(hMenu); i-- > 0;) {
 					api.GetMenuItemInfo(hMenu, i, true, mii);
 					if (mii.fState & MFS_DISABLED) {
-						var s = api.GetMenuString(hMenu, i, MF_BYPOSITION);
+						const s = api.GetMenuString(hMenu, i, MF_BYPOSITION);
 						if (s && s.indexOf(paste) == 0) {
 							api.EnableMenuItem(hMenu, i, MF_ENABLED | MF_BYPOSITION);
 							ExtraMenuCommand[mii.wID] = Sync.Badge.Paste;
@@ -462,12 +461,12 @@ AddEvent("Menus", function (Ctrl, hMenu, nPos, Selected, SelItem, ContextMenu, N
 AddEvent("ChangeNotify", function (Ctrl, pidls) {
 	if (Sync.Badge.DB) {
 		if (pidls.lEvent & (SHCNE_RENAMEFOLDER | SHCNE_RENAMEITEM)) {
-			var name = fso.GetFileName(api.GetDisplayNameOf(pidls[0], SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL));
-			var s = Sync.Badge.Remove(pidls[0]);
+			let name = GetFileName(api.GetDisplayNameOf(pidls[0], SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL));
+			let s = Sync.Badge.Remove(pidls[0]);
 			if (s) {
 				Sync.Badge.SetSync(name, s);
 			} else {
-				name = fso.GetFileName(api.GetDisplayNameOf(pidls[1], SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL));
+				name = GetFileName(api.GetDisplayNameOf(pidls[1], SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL));
 				s = Sync.Badge.SyncItem[name];
 			}
 			if (s) {
@@ -475,12 +474,12 @@ AddEvent("ChangeNotify", function (Ctrl, pidls) {
 			}
 		}
 		if (pidls.lEvent & (SHCNE_DELETE | SHCNE_RMDIR)) {
-			var name = fso.GetFileName(api.GetDisplayNameOf(pidls[0], SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL));
+			const name = GetFileName(api.GetDisplayNameOf(pidls[0], SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL));
 			Sync.Badge.SetSync(name, Sync.Badge.Remove(pidls[0]));
 		}
 		if (pidls.lEvent & (SHCNE_CREATE | SHCNE_MKDIR)) {
-			var name = fso.GetFileName(api.GetDisplayNameOf(pidls[0], SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL));
-			var Item = Sync.Badge.SyncItem[name];
+			const name = GetFileName(api.GetDisplayNameOf(pidls[0], SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL));
+			const Item = Sync.Badge.SyncItem[name];
 			if (Item) {
 				Sync.Badge.Set(pidls[0], Item);
 			}
@@ -508,11 +507,11 @@ AddEvent("BeginLabelEdit", function (Ctrl, Name) {
 }, true);
 
 AddEvent("ItemPostPaint2", function (Ctrl, pid, nmcd, vcd) {
-	var hList = Ctrl.hwndList;
+	const hList = Ctrl.hwndList;
 	if (hList && pid && Sync.Badge.DB) {
-		var image = Sync.Badge.Image[Sync.Badge.DB.Get(pid.Path)];
+		let image = Sync.Badge.Image[Sync.Badge.DB.Get(pid.Path)];
 		if (image) {
-			var rc = api.Memory("RECT");
+			const rc = api.Memory("RECT");
 			rc.left = LVIR_ICON;
 			api.SendMessage(hList, LVM_GETITEMRECT, nmcd.dwItemSpec, rc);
 			if (api.SendMessage(hList, LVM_GETVIEW, 0, 0) == 1) {
@@ -527,10 +526,10 @@ AddEvent("ItemPostPaint2", function (Ctrl, pid, nmcd, vcd) {
 });
 
 AddEvent("FilterChanged", function (Ctrl) {
-	var res = /^\*?badge:[^*]+/i.exec(Ctrl.FilterView);
+	let res = /^\*?badge:[^*]+/i.exec(Ctrl.FilterView);
 	if (res) {
 		Ctrl.OnIncludeObject = function (Ctrl, Path1, Path2, Item) {
-			var res = /^\*?badge:\s*([<=>]*)(\d+)/i.exec(Ctrl.FilterView);
+			res = /^\*?badge:\s*([<=>]*)(\d+)/i.exec(Ctrl.FilterView);
 			if (res) {
 				switch (res[1]) {
 					case '<':
@@ -555,9 +554,9 @@ AddEvent("Sort", function (Ctrl) {
 		clearTimeout(Sync.Badge.tid[Ctrl.Id]);
 		delete Sync.Badge.tid[Ctrl.Id];
 	}
-	if (/\-?Tablacus\.Badge$/i.test(Ctrl.SortColumn(1))) {
+	if (/\-?Tablacus\.Badge$/i.test(Ctrl.GetSortColumn(1))) {
 		Sync.Badge.tid[Ctrl.Id] = setTimeout(function () {
-			Sync.Badge.Sort(Ctrl, Ctrl.SortColumn(1));
+			Sync.Badge.Sort(Ctrl, Ctrl.GetSortColumn(1));
 		}, 99);
 	}
 });
@@ -568,22 +567,22 @@ AddEvent("Sorting", Sync.Badge.Sort);
 if (item.getAttribute("MenuExec")) {
 	AddEvent(item.getAttribute("Menu"), function (Ctrl, hMenu, nPos, Selected, item) {
 		if (item && item.IsFileSystem) {
-			var mii = api.Memory("MENUITEMINFO");
+			const mii = api.Memory("MENUITEMINFO");
 			mii.cbSize = mii.Size;
 			mii.fMask = MIIM_STRING | MIIM_SUBMENU;
 			mii.hSubMenu = api.CreatePopupMenu();
 			mii.dwTypeData = Sync.Badge.strName;
 			if (Selected && Selected.Count) {
-				var path = api.GetDisplayNameOf(Selected.Item(0), SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
+				const path = api.GetDisplayNameOf(Selected.Item(0), SHGDN_FORADDRESSBAR | SHGDN_FORPARSING | SHGDN_ORIGINAL);
 				if (path) {
-					var mii2 = api.Memory("MENUITEMINFO");
+					const mii2 = api.Memory("MENUITEMINFO");
 					mii2.cbSize = mii.Size;
 					mii2.fMask = MIIM_STRING | MIIM_SUBMENU;
 					mii2.hSubMenu = api.CreatePopupMenu();
 					mii2.dwTypeData = api.LoadString(hShell32, 12850);
-					var s = Sync.Badge.DB.Get(path);
-					var str1 = unescape('\%u2605%u2605%u2605%u2605%u2605%u2606%u2606%u2606%u2606%u2606%u2606');
-					for (var i = 6; i--;) {
+					const s = Sync.Badge.DB.Get(path);
+					const str1 = unescape('\%u2605%u2605%u2605%u2605%u2605%u2606%u2606%u2606%u2606%u2606%u2606');
+					for (let i = 6; i--;) {
 						api.InsertMenu(mii.hSubMenu, 0, MF_BYPOSITION | MF_STRING | (s == i ? MF_CHECKED : 0), ++nPos, ["&" + i, " - ", str1.substr(5 - i, 5)].join(""));
 						ExtraMenuCommand[nPos] = Sync.Badge["Set" + i];
 						api.InsertMenu(mii2.hSubMenu, 0, MF_BYPOSITION | MF_STRING, ++nPos, i ? ["&" + i, " - ", str1.substr(5 - i, 5)].join("") : GetText("All"));
@@ -610,10 +609,10 @@ if (item.getAttribute("MenuExec")) {
 	});
 }
 //Image
-var hdc, s;
-for (var i = 6; --i;) {
-	var image = api.CreateObject("WICBitmap");
-	if (s = api.PathUnquoteSpaces(ExtractMacro(te, item.getAttribute("Img" + i)))) {
+let hdc, hFont, s;
+for (let i = 6; --i;) {
+	const image = api.CreateObject("WICBitmap");
+	if (s = ExtractPath(te, item.getAttribute("Img" + i))) {
 		Sync.Badge.Image[i] = image.FromFile(s);
 		if (Sync.Badge.Image[i]) {
 			continue;
@@ -622,24 +621,26 @@ for (var i = 6; --i;) {
 	if (!hdc) {
 		hdc = api.GetDC(te.hwnd);
 	}
-	var rc = api.Memory("RECT");
-	var w = 32 * screen.deviceYDPI / 96;
+	const rc = api.Memory("RECT");
+	const w = 32 * screen.deviceYDPI / 96;
 	rc.right = w;
 	rc.bottom = w;
-	var hbm = api.CreateCompatibleBitmap(hdc, w, w);
-	var hmdc = api.CreateCompatibleDC(hdc);
-	var hOld = api.SelectObject(hmdc, hbm);
-	var brush = api.CreateSolidBrush(159 + i * 16);
+	const hbm = api.CreateCompatibleBitmap(hdc, w, w);
+	const hmdc = api.CreateCompatibleDC(hdc);
+	const hOld = api.SelectObject(hmdc, hbm);
+	const brush = api.CreateSolidBrush(159 + i * 16);
 	api.FillRect(hmdc, rc, brush);
 	api.DeleteObject(brush);
 	api.SetTextColor(hmdc, 0xffffff);
 	api.SetBkMode(hmdc, 1);
-	var lf = api.Memory("LOGFONT");
-	lf.lfFaceName = "Arial Black",
-	lf.lfHeight = -w;
-	lf.lfWeight = 700;
-	var hFont = CreateFont(lf);
-	var hfontOld = api.SelectObject(hmdc, hFont);
+	if (!hFont) {
+		const lf = api.Memory("LOGFONT");
+		lf.lfFaceName = "Arial Black",
+		lf.lfHeight = -w;
+		lf.lfWeight = 700;
+		hFont = CreateFont(lf);
+	}
+	const hfontOld = api.SelectObject(hmdc, hFont);
 	rc.top = -w / 4;
 	api.DrawText(hmdc, i, -1, rc, DT_CENTER);
 	api.SelectObject(hmdc, hfontOld);
