63e35f40a50c7ea741625f4ebda520c3001cd03b
Gaku <rxf10242@gmail.com>
2021-02-07 23:11:28 +0900

    21.2.7

---
+++ copyfoldersettings/script.js
@@ -0,0 +1,17 @@
+const Addon_Id = "copyfoldersettings";
+const item = await $.GetAddonElement(Addon_Id);
+if (!await item.getAttribute("Set")) {
+	item.setAttribute("MenuExec", 1);
+	item.setAttribute("Menu", "Background");
+	item.setAttribute("MenuPos", -1);
+
+	item.setAttribute("KeyOn", "List");
+	item.setAttribute("MouseOn", "List");
+
+	item.setAttribute("Format", "1");
+}
+if (window.Addon == 1) {
+	$.importScript("addons\\" + Addon_Id + "\\sync.js");
+} else {
+	SetTabContents(0, "General", await ReadTextFile("addons\\" + Addon_Id + "\\options.html"));
+}
---
+++ copyfoldersettings/sync.js
@@ -0,0 +1,38 @@
+const Addon_Id = "copyfoldersettings";
+const item = GetAddonElement(Addon_Id);
+
+Sync.CopyFolderSettings = {
+	strName: item.getAttribute("MenuName") || GetAddonInfo(Addon_Id).Name,
+	nPos: GetNum(item.getAttribute("MenuPos")),
+
+	Exec: function (Ctrl, pt) {
+		const FV = await te.Ctrl(CTRL_FV);
+		const s = ["FV.SetViewMode(", FV.CurrentViewMode, ",", FV.IconSize, ");\n"];
+		s.push("FV.Columns='", FV.GetColumns(Sync.CopyFolderSettings.Format), "';\n");
+		s.push("FV.GroupBy='", FV.GroupBy, "';\n");
+		if ((FV.SortColumns || "").split(/;/).length > 2 && FV.SortColumn != "System.Null") {
+			s.push("FV.SortColumns='", FV.SortColumns, "';\n");
+		} else {
+			s.push("FV.SortColumn='", FV.GetSortColumn(Sync.CopyFolderSettings.Format), "';\n");
+		}
+		clipboardData.setData("text", s.join(""));
+	}
+}
+//Menu
+if (item.getAttribute("MenuExec")) {
+	AddEvent(item.getAttribute("Menu"), function (Ctrl, hMenu, nPos, Selected, item) {
+		api.InsertMenu(hMenu, Sync.CopyFolderSettings.nPos, MF_BYPOSITION | MF_STRING, ++nPos, Sync.CopyFolderSettings.strName);
+		ExtraMenuCommand[nPos] = Sync.CopyFolderSettings.Exec;
+		return nPos;
+	});
+}
+//Key
+if (item.getAttribute("KeyExec")) {
+	SetKeyExec(item.getAttribute("KeyOn"), item.getAttribute("Key"), Sync.CopyFolderSettings.Exec, "Func");
+}
+//Mouse
+if (item.getAttribute("MouseExec")) {
+	SetGestureExec(item.getAttribute("MouseOn"), item.getAttribute("Mouse"), Sync.CopyFolderSettings.Exec, "Func");
+}
+
+AddTypeEx("Add-ons", "Copy folder settings", Sync.CopyFolderSettings.Exec);
