207ce2fca29918f4fd5a35b610320f564074b92f
Gaku <rxf10242@gmail.com>
2021-03-25 00:11:07 +0900

    21.3.24

--- innerfilterbar/script.js
+++ innerfilterbar/script.js
@@ -6,7 +6,6 @@ if (window.Addon == 1) {
 		filter: [],
 		iCaret: [],
 		Width: GetWidth(item.getAttribute("Width")) || '176px',
-		Icon: item.getAttribute("Icon") ? await ExtractPath(te, item.getAttribute("Icon")) : await MakeImgSrc("bitmap:comctl32.dll,140,13,0", 0, false, 13),
 		IconS: item.getAttribute("Icon") ? 16 : 13,
 		RE: item.getAttribute("RE"),
 
@@ -145,13 +144,22 @@ if (window.Addon == 1) {
 				Addons.FilterList.Exec(o, null, id);
 			}
 			return false;
-		}
+		},
+
+		MouseDown: function (ev) {
+			Addons.InnerFilterBar.ptDown = { x: ev.screenX, y: ev.screenY };
+		},
+
+		MouseUp: function (ev, o, id) {
+			if (ev.screenX == Addons.InnerFilterBar.ptDown.x && ev.screenY == Addons.InnerFilterBar.ptDown.y) {
+				Addons.InnerFilterBar.FilterList(o, id);
+			}
+		},
 	};
 
 	AddEvent("PanelCreated", function (Ctrl, Id) {
 		const z = screen.deviceYDPI / 96;
-		const nSize = Addons.InnerFilterBar.IconS;
-		const s = ['<input type="text" name="filter_', Id, '" placeholder="Filter" onkeydown="return Addons.InnerFilterBar.KeyDown(event, this,', Id, ')"  onkeyup="return Addons.InnerFilterBar.KeyUp(event,', Id, ')" onmouseup="Addons.InnerFilterBar.KeyDown(event, this,', Id, ')" onfocus="Addons.InnerFilterBar.Focus(this,', Id, ')" onblur="Addons.InnerFilterBar.ShowButton(this,', Id, ')" ondblclick="return Addons.InnerFilterBar.FilterList(this,', Id, ')" style="width: ', EncodeSC(Addons.InnerFilterBar.Width), '; padding-right: ', nSize * z, 'px; vertical-align: middle"><span style="position: relative"><input type="image" src="', EncodeSC(Addons.InnerFilterBar.Icon), '" id="ButtonFilter_', Id, '" hidefocus="true" style="position: absolute; left:', -18 * z, 'px; top:', (18 - nSize) / 2 * z, 'px; width: ', nSize * z, 'px; height: ', nSize * z, 'px" onclick="return Addons.InnerFilterBar.FilterList(this,', Id, ')" oncontextmenu="return Addons.InnerFilterBar.FilterList(this,', Id, ')"><span id="ButtonFilterClear_', Id, '" style="font-family: marlett; font-size:', 9 * z, 'px; display: none; position: absolute; left:', -28 * z, 'px; top: ', 4 * z, 'px" class="button" onclick="Addons.InnerFilterBar.Clear(true,', Id, ')">r</span></span>'];
+		const s = ['<input type="text" name="filter_', Id, '" placeholder="Filter" onkeydown="return Addons.InnerFilterBar.KeyDown(event, this,', Id, ')"  onkeyup="return Addons.InnerFilterBar.KeyUp(event,', Id, ')" onmouseup="Addons.InnerFilterBar.KeyDown(event, this,', Id, ')" onfocus="Addons.InnerFilterBar.Focus(this,', Id, ')" onblur="Addons.InnerFilterBar.ShowButton(this,', Id, ')" ondblclick="return Addons.InnerFilterBar.FilterList(this,', Id, ', 1)" style="width: ', EncodeSC(Addons.InnerFilterBar.Width), '; padding-right: ', Addons.InnerFilterBar.IconS * z, 'px; vertical-align: middle"><span style="position: relative">', Addons.InnerFilterBar.Icon.replace(/\$/g, Id), '<span id="ButtonFilterClear_', Id, '" style="font-family: marlett; font-size:', 9 * z, 'px; display: none; position: absolute; left:', -28 * z, 'px; top: ', 4 * z, 'px" class="button" onclick="Addons.InnerFilterBar.Clear(true,', Id, ')">r</span></span>'];
 		SetAddon(null, "Inner1Right_" + Id, s.join(""));
 		(async function () {
 			Addons.InnerFilterBar.GetFilter(await Ctrl.Selected);
@@ -174,6 +182,18 @@ if (window.Addon == 1) {
 		SetGestureExec(item.getAttribute("MouseOn"), item.getAttribute("Mouse"), Addons.InnerFilterBar.Exec, "Async");
 	}
 	AddTypeEx("Add-ons", "Inner Filter Bar", Addons.InnerFilterBar.Exec);
+
+	const z = screen.deviceYDPI / 96;
+	const h = Addons.InnerFilterBar.IconS;
+	const src = item.getAttribute("Icon") ? await ExtractPath(te, item.getAttribute("Icon")) : (WINVER >= 0xa00 ? "font:Segoe MDL2 Assets,0xe71c" : "bitmap:comctl32.dll,140,13,0");
+	Addons.InnerFilterBar.Icon = await GetImgTag({
+		id: "ButtonFilter_$",
+		style: ['position: absolute; left:', -18 * z, 'px; top:', (18 - h) / 2 * z, 'px; cursor: default; color:', GetWebColor(await GetSysColor(COLOR_WINDOWTEXT))].join(""),
+		onmousedown: "Addons.InnerFilterBar.MouseDown(event); return false",
+		onmouseup: "Addons.InnerFilterBar.MouseUp(event, this, $)",
+		oncontextmenu: "return false;",
+		src: src
+	}, h * z);
 } else {
 	SetTabContents(0, "General", '<table style="width: 100%"><tr><td><label>Width</label></td></tr><tr><td><input type="text" name="Width" size="10"></td><td><input type="button" value="Default" onclick="document.F.Width.value=\'\'"></td></tr><tr><td><label>Filter</label></td></tr><tr><td><input type="checkbox" id="RE" name="RE"><label for="RE">Regular Expression</label>/<label for="RE">Migemo</label></td></tr></table>');
 	ChangeForm([["__IconSize", "style/display", "none"]]);
