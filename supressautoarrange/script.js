c35ded569e4ec9a83406690543bfc2d32214ac73
Gaku <rxf10242@gmail.com>
2021-04-25 22:05:14 +0900

    21.4.25

--- supressautoarrange/script.js
+++ supressautoarrange/script.js
@@ -1,25 +1,25 @@
 if (window.Addon == 1) {
-	Addons.PreventAutoArrange = {
+	Addons.SupressAutoArrange = {
 		hModule: await api.LoadLibraryEx(BuildPath(ui_.Installed, "addons\\supressautoarrange\\tsaa" + ui_.bit + ".dll"), 0, 0),
 		Finalize: function () {
-			if (Addons.PreventAutoArrange.hModule) {
-				te.RemoveEvent("MessageSFVCB", Addons.PreventAutoArrange.Proc);
-				api.FreeLibrary(Addons.PreventAutoArrange.hModule);
-				delete Addons.PreventAutoArrange.hModule;
+			if (Addons.SupressAutoArrange.hModule) {
+				te.RemoveEvent("MessageSFVCB", Addons.SupressAutoArrange.Proc);
+				api.FreeLibrary(Addons.SupressAutoArrange.hModule);
+				delete Addons.SupressAutoArrange.hModule;
 			}
 		}
 	}
 
-	const hModule = Addons.PreventAutoArrange.hModule;
+	const hModule = Addons.SupressAutoArrange.hModule;
 	if (hModule) {
-		Addons.PreventAutoArrange.Proc = await api.GetProcAddress(hModule, "MessageSFVCB");
-		te.AddEvent("MessageSFVCB", Addons.PreventAutoArrange.Proc);
+		Addons.SupressAutoArrange.Proc = await api.GetProcAddress(hModule, "MessageSFVCB");
+		te.AddEvent("MessageSFVCB", Addons.SupressAutoArrange.Proc);
 
-		AddEvent("Finalize", Addons.PreventAutoArrange.Finalize);
+		AddEvent("Finalize", Addons.SupressAutoArrange.Finalize);
 
 		AddEvent("AddonDisabled", function (Id) {
-			if (SameText(Id, "PreventAutoArrange")) {
-				Addons.PreventAutoArrange.Finalize();
+			if (SameText(Id, "supressautoarrange")) {
+				Addons.SupressAutoArrange.Finalize();
 			}
 		});
 	}
