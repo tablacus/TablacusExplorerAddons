207be8cd5c45ed98521f5b6bd7604e8e8291fada
Gaku <rxf10242@gmail.com>
2021-08-20 22:14:22 +0900

    21.8.20

--- tabcolor/script.js
+++ tabcolor/script.js
@@ -1,6 +1,6 @@
-var Addon_Id = "tabcolor";
-var item = await GetAddonElement(Addon_Id);
-if (!item.getAttribute("Set")) {
+const Addon_Id = "tabcolor";
+const item = await $.GetAddonElement(Addon_Id);
+if (!await item.getAttribute("Set")) {
 	item.setAttribute("MenuExec", 1);
 	item.setAttribute("Menu", "Tabs");
 	item.setAttribute("MenuPos", 0);
@@ -12,5 +12,5 @@ if (!item.getAttribute("Set")) {
 	item.setAttribute("Mouse", "");
 }
 if (window.Addon == 1) {
-	importJScript("addons\\" + Addon_Id + "\\sync.js");
+	$.importScript("addons\\" + Addon_Id + "\\sync.js");
 }
