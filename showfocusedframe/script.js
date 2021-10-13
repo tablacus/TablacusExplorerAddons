808f22b8362b04cfb5f3f3accded9534940b9aa9
Gaku <rxf10242@gmail.com>
2021-10-13 22:12:43 +0900

    21.10.13

--- showfocusedframe/script.js
+++ showfocusedframe/script.js
@@ -1,9 +1,18 @@
 if (window.Addon == 1) {
-	AddEvent("NavigateComplete", async function (Ctrl) {
-		const hList = await Ctrl.hwndList;
-		if (hList) {
-			api.PostMessage(hList, WM_KEYDOWN, VK_CLEAR, 0);
-			api.PostMessage(hList, WM_KEYUP, VK_CLEAR, 0);
+	Addons.ShowFocusedFrame = {
+		Exec: async function (Ctrl) {
+			const hList = await Ctrl.hwndList;
+			if (hList) {
+				api.PostMessage(hList, WM_KEYDOWN, VK_CLEAR, 0);
+				api.PostMessage(hList, WM_KEYUP, VK_CLEAR, 0);
+			}
 		}
-	});
+	}
+	AddEvent("NavigateComplete", Addons.ShowFocusedFrame.Exec);
+
+	AddEvent("KeyMessage", function (Ctrl, hwnd, msg, key, keydata) {
+		if (key == VK_MENU && msg == WM_KEYUP) {
+			Addons.ShowFocusedFrame.Exec(Ctrl);
+		}
+	}, true);
 }
