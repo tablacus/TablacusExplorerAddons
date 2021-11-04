6185c6a92f018e7e390533bf528f96eb3ee6d2b7
Gaku <rxf10242@gmail.com>
2021-11-04 21:41:01 +0900

    21.11.4

--- fixwin11prev/sync.js
+++ fixwin11prev/sync.js
@@ -1,44 +1,32 @@
+Sync.FixWin11Preview = {
+	rc: api.Memory("RECT")
+}
+
+AddEvent("ToolTip", function (Ctrl, Index) {
+	if (Ctrl.Type == CTRL_SB && Index >= 0) {
+		api.GetCursorPos(Sync.FixWin11Preview.pt);
+		Sync.FixWin11Preview.rc.left = LVIR_SELECTBOUNDS;
+		api.SendMessage(Ctrl.hwndList, LVM_GETITEMRECT, Index, Sync.FixWin11Preview.rc);
+		Sync.FixWin11Preview.hList = Ctrl.hwndList;
+	}
+});
+
 AddEvent("MouseMessage", function (Ctrl, hwnd, msg, wParam, pt) {
-	if (Ctrl.Type == CTRL_SB) {
-		if (msg == WM_LBUTTONDOWN || msg == WM_RBUTTONDOWN) {
-			const hList = Ctrl.hwndList;
-			let iItem = Ctrl.HitTest(pt, LVHT_ONITEM);
-			if (iItem >= 0) {
-				const rc = api.Memory("RECT");
-				rc.left = LVIR_SELECTBOUNDS;
-				api.SendMessage(hList, LVM_GETITEMRECT, iItem, rc);
-				const ptc = pt.Clone();
-				api.ScreenToClient(hList, ptc);
-				if (!PtInRect(rc, ptc)) {
-					for (iItem = Ctrl.ItemCount(SVGIO_ALLVIEW); --iItem >= 0;) {
-						rc.left = LVIR_SELECTBOUNDS;
-						api.SendMessage(hList, LVM_GETITEMRECT, iItem, rc);
-						if (PtInRect(rc, ptc)) {
-							if (api.GetKeyState(VK_SHIFT) < 0) {
-								Ctrl.SelectItem(iItem, SVSI_FOCUSED | SVSI_ENSUREVISIBLE | SVSI_DESELECTOTHERS | SVSI_SELECT);
-								let i = api.SendMessage(hList, 4162, 0, 0), j = iItem;
-								if (i > j) {
-									j = i;
-									i = iItem;
-								}
-								while (i < j) {
-									Ctrl.SelectItem(i++, SVSI_SELECT);
-								}
-							} else {
-								let wFlags = SVSI_FOCUSED | SVSI_ENSUREVISIBLE | SVSI_SELECTIONMARK;
-								if (api.GetKeyState(VK_CONTROL) < 0) {
-									if (api.SendMessage(hList, LVM_GETNEXTITEM, iItem - 1, LVNI_ALL | LVNI_SELECTED) != iItem) {
-										wFlags |= SVSI_SELECT;
-									}
-								} else {
-									wFlags |= SVSI_DESELECTOTHERS | SVSI_SELECT;
-								}
-								Ctrl.SelectItem(iItem, wFlags);
-							}
-							return S_OK;
-						}
-					}
+	if (msg == WM_MOUSEMOVE && Ctrl.Type == CTRL_SB && Ctrl.hList == Sync.FixWin11Preview.hList) {
+		let hwnd, hwnd1;
+		while (hwnd1 = api.FindWindowEx(null, hwnd1, null, null)) {
+			if (api.GetClassName(hwnd1) == "tooltips_class32") {
+				if (api.IsWindowVisible(hwnd1)) {
+					hwnd = hwnd1;
+					break;
 				}
+			}
+		}
+		if (hwnd) {
+			const ptc = pt.Clone();
+			api.ScreenToClient(Sync.FixWin11Preview.hList, ptc);
+			if (!PtInRect(Sync.FixWin11Preview.rc, ptc)) {
+				api.ShowWindow(hwnd, SW_HIDE);
 			}
 		}
 	}
