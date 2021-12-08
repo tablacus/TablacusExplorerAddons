a981a47a133632e65f8cae7ec27542a2f3c08b9a
Gaku <rxf10242@gmail.com>
2021-12-08 22:47:49 +0900

    21.12.8

--- darkmode/script.js
+++ darkmode/script.js
@@ -1,6 +1,48 @@
 const Addon_Id = "darkmode";
 
+Addons.DarkMode = {
+	SetCss: function () {
+		Promise.all([MainWindow.Sync.DarkMode.css, api.ShouldAppsUseDarkMode(), MainWindow.Sync.DarkMode.Auto]).then(function (r) {
+			const style = document.createElement("style");
+			style.media = "screen";
+			if (r[2]) {
+				r[0] = "@media (prefers-color-scheme: dark) {" + r[0] + "}";
+			}
+			style.appendChild(document.createTextNode(r[0]));
+			if (r[1] || r[2]) {
+				document.head.appendChild(style);
+			}
+		});
+	},
+
+	SetCssIE: function () {
+		const style = document.createElement("style");
+		style.media = "screen";
+		style.appendChild(document.createTextNode(MainWindow.Sync.DarkMode.css));
+		if (window.Addons && Addons.DarkMode) {
+			Addons.DarkMode.node = style;
+		}
+		if (MainWindow.Sync.DarkMode.bDark) {
+			document.head.appendChild(style);
+		}
+	},
+
+	RefreshCss: function () {
+		if (MainWindow.Sync.DarkMode.bDark) {
+			document.head.appendChild(Addons.DarkMode.node);
+		} else {
+			document.head.removeChild(Addons.DarkMode.node);
+		}
+	},
+
+	InitBG: async function () {
+		InitBG(await GetWinColor(window.getComputedStyle ? getComputedStyle(document.body).getPropertyValue('background-color') : document.body.currentStyle.backgroundColor));
+		api.RedrawWindow(ui_.hwnd, null, 0, RDW_NOERASE | RDW_INVALIDATE | RDW_ALLCHILDREN);
+	}
+};
+
 if (window.Addon == 1) {
+	AddEvent("BrowserCreatedEx", (window.chrome ? Addons.DarkMode.SetCss : Addons.DarkMode.SetCssIE).toString().replace(/^[^{]+{|}$/g, ""), true);
 	$.importScript("addons\\" + Addon_Id + "\\sync.js");
 } else {
 	SetTabContents(0, "General", '<label><input type="checkbox" id="Auto">Auto</label><br>');
--- darkmode/sync.js
+++ darkmode/sync.js
@@ -1,28 +1,8 @@
 const Addon_Id = "darkmode";
 
-if (!api.ShouldAppsUseDarkMode() && GetAddonOptionEx(Addon_Id, "Auto")) {
-	return;
-}
-
 Sync.DarkMode = {
-	clrText: 0xffffff,
-	clrBk: 0x202020,
-	clrLine: 0x555555,
-
-	SetCss: function () {
-		const link = document.createElement("link");
-		link.rel = "stylesheet";
-		link.type = "text/css";
-		link.href = "style.css";
-		document.head.appendChild(link);
-	},
-
-	SetCssIE: function () {
-		const style = document.createElement("style");
-		style.media = "screen";
-		style.appendChild(document.createTextNode(ReadTextFile(BuildPath(te.Data.Installed, "addons\\darkmode\\style.css"))));
-		document.head.appendChild(style);
-	},
+	Auto: GetAddonOptionEx(Addon_Id, "Auto"),
+	css: ReadTextFile(BuildPath(te.Data.Installed, "addons\\darkmode\\style.css")),
 
 	Arrange: function (Ctrl) {
 		const FV = GetFolderView(Ctrl);
@@ -53,7 +33,7 @@ Sync.DarkMode = {
 		}
 	},
 
-	Init: function () {
+	Refresh: function () {
 		const cFV = te.Ctrls(CTRL_FV);
 		for (let i in cFV) {
 			this.Arrange(cFV[i]);
@@ -61,15 +41,47 @@ Sync.DarkMode = {
 	}
 }
 
-if (window.chrome) {
-	AddEvent("BrowserCreatedEx", Sync.DarkMode.SetCss.toString().replace(/^[^{]+{|}$/g, "").replace("style\.css", api.UrlCreateFromPath(BuildPath(te.Data.Installed, "addons\\darkmode\\style.css"))), true);
-} else {
-	AddEvent("BrowserCreatedEx", Sync.DarkMode.SetCssIE.toString().replace(/^[^{]+{|}$/g, ""), true);
-}
+Sync.DarkMode.bDark = api.ShouldAppsUseDarkMode() || !Sync.DarkMode.Auto;
+
+AddEvent("SystemMessage", function (Ctrl, hwnd, msg, wParam, lParam) {
+	if (msg == WM_SETTINGCHANGE) {
+		const bDark = api.ShouldAppsUseDarkMode() || !Sync.DarkMode.Auto;
+		if (bDark != Sync.DarkMode.bDark) {
+			Sync.DarkMode.bDark = bDark;
+			Sync.DarkMode.Init();
+			if (!window.chrome) {
+				Addons.DarkMode.RefreshCss();
+			}
+			InvokeUI("Addons.DarkMode.InitBG")
+		}
+	}
+});
 
 if (Sync.Color) {
+	Sync.DarkMode.Init = function () {};
 	return;
 }
+
+Sync.DarkMode.Init = function () {
+	if (Sync.DarkMode.bDark) {
+		Sync.DarkMode.clrText = 0xffffff;
+		Sync.DarkMode.clrBk = 0x202020;
+		Sync.DarkMode.clrLine = 0x555555;
+		Sync.DarkMode.clrBtnFace = 0x2c2c2c;
+		SetSysColor(COLOR_WINDOWTEXT, Sync.DarkMode.clrText);
+		SetSysColor(COLOR_WINDOW, Sync.DarkMode.clrBk);
+		SetSysColor(COLOR_BTNFACE, Sync.DarkMode.clrBtnFace);
+	} else {
+		SetSysColor(COLOR_WINDOWTEXT, void 0);
+		SetSysColor(COLOR_WINDOW, void 0);
+		SetSysColor(COLOR_BTNFACE, void 0);
+		Sync.DarkMode.clrText = GetSysColor(COLOR_WINDOWTEXT);
+		Sync.DarkMode.clrBk = GetSysColor(COLOR_WINDOW);
+		Sync.DarkMode.clrLine = GetSysColor(COLOR_WINDOWTEXT);
+	}
+	Sync.DarkMode.Refresh();
+}
+
 
 AddEvent("ViewCreated", Sync.DarkMode.Arrange);
 
@@ -88,12 +100,7 @@ AddEvent("Create", function (Ctrl) {
 });
 
 AddEventId("AddonDisabledEx", "darkmode", function () {
-	SetSysColor(COLOR_WINDOWTEXT, void 0);
-	SetSysColor(COLOR_WINDOW, void 0);
-	SetSysColor(COLOR_BTNFACE, void 0);
-	Sync.DarkMode.clrText = GetSysColor(COLOR_WINDOWTEXT);
-	Sync.DarkMode.clrBk = GetSysColor(COLOR_WINDOW);
-	Sync.DarkMode.clrLine = GetSysColor(COLOR_WINDOWTEXT);
+	Sync.DarkMode.bDark = false;
 	Sync.DarkMode.Init();
 });
 
@@ -101,20 +108,19 @@ if (api.IsAppThemed() && WINVER > 0x603) {
 	AddEvent("Load", function () {
 		if (!Sync.ClassicStyle) {
 			AddEvent("ItemPrePaint", function (Ctrl, pid, nmcd, vcd, plRes) {
-				const uState = api.SendMessage(Ctrl.hwndList, LVM_GETITEMSTATE, nmcd.dwItemSpec, LVIS_SELECTED);
-				if (uState & LVIS_SELECTED || nmcd.uItemState & CDIS_HOT) {
-					const rc = api.Memory("RECT");
-					rc.left = LVIR_SELECTBOUNDS;
-					api.SendMessage(Ctrl.hwndList, LVM_GETITEMRECT, nmcd.dwItemSpec, rc);
-					api.SetDCBrushColor(nmcd.hdc, 0x555555);
-					api.FillRect(nmcd.hdc, rc, api.GetStockObject(DC_BRUSH));
+				if (Sync.DarkMode.bDark) {
+					const uState = api.SendMessage(Ctrl.hwndList, LVM_GETITEMSTATE, nmcd.dwItemSpec, LVIS_SELECTED);
+					if (uState & LVIS_SELECTED || nmcd.uItemState & CDIS_HOT) {
+						const rc = api.Memory("RECT");
+						rc.left = LVIR_SELECTBOUNDS;
+						api.SendMessage(Ctrl.hwndList, LVM_GETITEMRECT, nmcd.dwItemSpec, rc);
+						api.SetDCBrushColor(nmcd.hdc, 0x555555);
+						api.FillRect(nmcd.hdc, rc, api.GetStockObject(DC_BRUSH));
+					}
 				}
 			}, true);
 		}
 	});
 }
 
-SetSysColor(COLOR_WINDOWTEXT, Sync.DarkMode.clrText);
-SetSysColor(COLOR_WINDOW, Sync.DarkMode.clrBk);
-SetSysColor(COLOR_BTNFACE, 0x2c2c2c);
 Sync.DarkMode.Init();
