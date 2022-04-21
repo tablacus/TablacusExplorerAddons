62c462b444af2561463933ef519536c7de9ad5cb
Gaku <rxf10242@gmail.com>
2022-04-21 22:19:40 +0900

    22.4.21

--- openinstead/sync.js
+++ openinstead/sync.js
@@ -7,14 +7,28 @@ Sync.OpenInstead = {
 	TakeOver: item.getAttribute("TakeOver"),
 
 	Exec: function () {
-		try {
-			const sw = sha.Windows();
-			for (let i = sw.Count; i-- > 0;) {
-				const exp = sw.item(i);
-				if (exp && exp.Visible && !exp.Busy) {
-					const doc = exp.Document;
-					if (doc) {
-						let path = api.GetDisplayNameOf(doc, SHGDN_FORADDRESSBAR | SHGDN_FORPARSING);
+		const o = api.CreateObject("Object");
+		o.Data = api.CreateObject("Object");
+		o.Data.Sync = Sync;
+		o.Data.hwnd = te.hwnd;
+		o.Data.MainWindow = window;
+		o.Data.uFlags = SHGDN_FORADDRESSBAR | SHGDN_FORPARSING;
+		o.Data.wFlags = SBSP_NEWBROWSER;
+		o.Data.dwFlags = SVSI_FOCUSED | SVSI_ENSUREVISIBLE | SVSI_DESELECTOTHERS | SVSI_SELECTIONMARK | SVSI_SELECT;
+		api.ExecScript(FixScript(Sync.OpenInstead.Worker.toString().replace(/^[^{]+{|}$/g, "")), "JScript", o, true);
+		return S_OK;
+	},
+
+	Worker: function () {
+		const sha = api.CreateObject("sha");
+		const sw = sha.Windows();
+		for (let i = sw.Count; i-- > 0;) {
+			let exp = sw.item(i);
+			if (exp && exp.Visible && !exp.Busy) {
+				let doc = exp.Document;
+				if (doc) {
+					try {
+						let path = api.GetDisplayNameOf(doc, uFlags);
 						let url = doc;
 						if (!path && /\\explorer\.exe$/i.test(exp.FullName)) {
 							path = api.PathCreateFromUrl(exp.LocationURL);
@@ -22,7 +36,7 @@ Sync.OpenInstead = {
 						}
 						if (Sync.OpenInstead.Match(path)) {
 							exp.Visible = false;
-							const FV = te.Ctrl(CTRL_FV).Navigate(api.ILCreateFromPath(url).ExtendedProperty("linktarget") || url, SBSP_NEWBROWSER);
+							let FV = MainWindow.GetFolderView().Navigate(api.ILCreateFromPath(url).ExtendedProperty("linktarget") || url, wFlags);
 							if (Sync.OpenInstead.TakeOver) {
 								FV.CurrentViewMode = doc.CurrentViewMode;
 								if (doc.IconSize) {
@@ -31,16 +45,19 @@ Sync.OpenInstead = {
 								if (doc.SortColumns) {
 									FV.SortColumns = doc.SortColumns;
 								}
+								if (doc.GroupBy) {
+									FV.GroupBy = doc.GroupBy;
+								}
 							}
-							FV.SelectItem(doc.FocusedItem, SVSI_FOCUSED | SVSI_ENSUREVISIBLE | SVSI_DESELECTOTHERS | SVSI_SELECTIONMARK | SVSI_SELECT);
+							FV.SelectItem(doc.FocusedItem, dwFlags);
 							exp.Quit();
-							RestoreFromTray();
-							api.SetForegroundWindow(te.hwnd);
+							MainWindow.RestoreFromTray();
+							api.SetForegroundWindow(hwnd);
 						}
-					}
+					} catch (e) { }
 				}
 			}
-		} catch (e) { }
+		}
 	},
 
 	Match: function (path) {
