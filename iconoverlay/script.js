29e1a6af7b8cc313af8a924f219935ad98ab700f
Gaku <rxf10242@gmail.com>
2024-02-09 22:39:00 +0900

    24.2.9

--- iconoverlay/script.js
+++ iconoverlay/script.js
@@ -13,5 +13,5 @@ if (window.Addon == 1) {
 	};
 	$.importScript("addons\\" + Addon_Id + "\\sync.js");
 } else {
-	SetTabContents(0, "General", '<label>Base</label><br><input type="text" id="Base" placeholder="11" style="width: 100%">');
+	SetTabContents(0, "", await ReadTextFile("addons\\" + Addon_Id + "\\options.html"));
 }
