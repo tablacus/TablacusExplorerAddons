04c246adfc6bca7a054078352d602867ff53b2d3
Gaku <rxf10242@gmail.com>
2024-02-29 21:56:56 +0900

    24.2.29

--- sidetreeview/sync.js
+++ sidetreeview/sync.js
@@ -38,8 +38,23 @@ Sync.SideTreeView = {
 	}
 }
 
-AddEvent("ChangeNotify", function (Ctrl, pidls, wParam, lParam) {
-	if (pidls.lEvent & (SHCNE_MKDIR | SHCNE_RMDIR | SHCNE_RENAMEFOLDER)) {
-		Common.SideTreeView.TV.Notify(pidls.lEvent, pidls[0], pidls[1], wParam, lParam);
+AddEvent("AppMessage", function (Ctrl, hwnd, msg, wParam, lParam) {
+	if (msg == Common.SideTreeView.WM) {
+		var pidls = {};
+		var hLock = api.SHChangeNotification_Lock(wParam, lParam, pidls);
+		if (hLock) {
+			api.SHChangeNotification_Unlock(hLock);
+			if (pidls[0] && /^[A-Z]:\\|^\\\\\w/i.test(pidls[0].Path) && IsWitness(pidls[0])) {
+				Common.SideTreeView.TV.Notify(pidls.lEvent, pidls[0], pidls[1], wParam, lParam);
+			}
+		}
+		return S_OK;
 	}
 });
+
+AddEvent("Finalize", function () {
+	api.SHChangeNotifyDeregister(Common.SideTreeView.uRegisterId);
+});
+
+Common.SideTreeView.WM = TWM_APP++;
+Common.SideTreeView.uRegisterId = api.SHChangeNotifyRegister(te.hwnd, SHCNRF_InterruptLevel | SHCNRF_NewDelivery, SHCNE_MKDIR | SHCNE_MEDIAINSERTED | SHCNE_DRIVEADD | SHCNE_NETSHARE | SHCNE_DRIVEREMOVED | SHCNE_MEDIAREMOVED | SHCNE_NETUNSHARE | SHCNE_RENAMEFOLDER | SHCNE_RMDIR | SHCNE_SERVERDISCONNECT | SHCNE_UPDATEDIR, Common.SideTreeView.WM, ssfDESKTOP, true);
