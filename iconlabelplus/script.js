2cd996d7ba55ad6b1e093641ff8526a8c3d3d6ee
Gaku <rxf10242@gmail.com>
2024-05-06 22:25:07 +0900

    23.5.6

--- iconlabelplus/script.js
+++ iconlabelplus/script.js
@@ -1,11 +1,4 @@
 const Addon_Id = "iconlabelplus";
 if (window.Addon == 1) {
-	if (WINVER >= 0x600 && await api.IsAppThemed()) {
-		AddEvent("Load", function () {
-			if (!Addon.ClassicStyle) {
-				Sync.IconLabelPlus.SetStyle();
-			}
-		});
-	}
 	$.importScript("addons\\" + Addon_Id + "\\sync.js");
 }
