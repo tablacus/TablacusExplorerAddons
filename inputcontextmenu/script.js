3ea8c223b4f5f9b6e4a633beb0ce3770e1ae75b7
Gaku <rxf10242@gmail.com>
2025-05-04 21:58:57 +0900

    25.5.4

--- inputcontextmenu/script.js
+++ inputcontextmenu/script.js
@@ -1,3 +1,10 @@
 if (window.Addon == 1) {
 	AddEvent("BrowserCreatedEx", await ReadTextFile("addons\\inputcontextmenu\\handler.js"));
+	document.addEventListener("keydown", async function (ev) {
+		if (ev.ctrlKey && (ev.key ? ev.key.toLowerCase() === 'f' : ev.keyCode == 70)) {
+			PreventDefault(ev);
+			const wb = await WebBrowser;
+			KeyExecEx(wb, "All", 8225, await wb.hwnd)
+		}
+	}, true)
 }
