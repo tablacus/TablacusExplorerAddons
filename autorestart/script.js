4901f034a12eb8ae88f98ec4793c51d41d20f942
Gaku <rxf10242@gmail.com>
2025-06-10 21:52:48 +0900

    25.6.10

--- autorestart/script.js
+++ autorestart/script.js
@@ -19,7 +19,7 @@ if (window.Addon == 1) {
 						te.CmdShow = SW_SHOWNOACTIVATE;
 						te.Data.bSaveConfig = true;
 					}
-					await SaveConfig();
+					await FinalizeUI();
 					te.Reload(true);
 				}, Addons.AutoRestart.Interval);
 			}
@@ -28,6 +28,8 @@ if (window.Addon == 1) {
 	if (Addons.AutoRestart.Interval >= 60000) {
 		AddEvent("MouseMessage", Addons.AutoRestart.Exec, true);
 		AddEvent("KeyMessage", Addons.AutoRestart.Exec, true);
+		window.addEventListener("keydown", Addons.AutoRestart.Exec);
+		window.addEventListener("mouseover", Addons.AutoRestart.Exec);
 	}
 } else {
 	SetTabContents(0, "General", '<table><td><input type="text" name="hour" style="width: 3em">:<input type="text" name="min" style="width: 3em"></td><td></td></tr></table>');
