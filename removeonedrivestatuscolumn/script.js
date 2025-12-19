ed489906a58cb730cdcd3865305f04530324aab7
Logan <6226581+LoganAC34@users.noreply.github.com>
2025-12-19 10:54:59 -0500

    Added "Remove OneDrive Status Column" add-on

---
+++ removeonedrivestatuscolumn/script.js
@@ -0,0 +1,18 @@
+﻿if (window.Addon == 1) {
+	Addons.RemoveStatusColumn = {
+		Status: '"' + await api.PSGetDisplayName("System.StorageProviderUIStatus") + '"',
+
+		Exec: async function (Ctrl) {
+			const s = await Ctrl.Columns;
+			if (s.indexOf(Addons.RemoveStatusColumn.Status) >= 0) {
+				var re = new RegExp(Addons.RemoveStatusColumn.Status + " \\d+", "g");
+				Ctrl.Columns = s.replace(re, "");
+			}
+		}
+	};
+
+	if (Addons.RemoveStatusColumn.Status != '""') {
+		AddEvent("NavigateComplete", Addons.RemoveStatusColumn.Exec);
+		AddEvent("ColumnsChanged", Addons.RemoveStatusColumn.Exec);
+	}
+}
